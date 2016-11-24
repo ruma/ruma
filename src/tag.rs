422afdc2f2c5f4035c548e87cc354e1b8e455a1f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-11-24 07:18:59 -0800

    TagInfo's order field should be a string. Fixes #4.

--- src/tag.rs
+++ src/tag.rs
@@ -19,5 +19,5 @@ pub struct TagEventContent {
 pub struct TagInfo {
     /// Value to use for lexicographically ordering rooms with this tag.
     #[serde(skip_serializing_if="Option::is_none")]
-    pub order: Option<u64>,
+    pub order: Option<String>,
 }
