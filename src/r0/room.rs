5c43a32b007ccd77298136c4a9dd2ce31644f91f
Jonas Platte <jplatte+git@posteo.de>
2017-07-02 06:01:12 +1000

    Use ruma-api-macros for the create_room endpoint

--- src/r0/room.rs
+++ src/r0/room.rs
@@ -3,26 +3,40 @@
 /// [POST /_matrix/client/r0/createRoom](https://matrix.org/docs/spec/client_server/r0.2.0.html#post-matrix-client-r0-createroom)
 pub mod create_room {
     use ruma_identifiers::{RoomId, UserId};
+    use ruma_api_macros::ruma_api;
 
-    /// The request type.
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct BodyParams {
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub creation_content: Option<CreationContent>,
-        #[serde(skip_serializing_if = "Vec::is_empty")]
-        #[serde(default)]
-        pub invite: Vec<UserId>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub name: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub preset: Option<RoomPreset>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub room_alias_name: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub topic: Option<String>,
-        #[serde(skip_serializing_if = "Option::is_none")]
-        pub visibility: Option<String>, // TODO: should be an enum ["public", "private"]
-        // TODO: missing `invite_3pid`, `initial_state`
+    ruma_api! {
+        metadata {
+            description: "Create a new room.",
+            method: Method::Post,
+            name: "create_room",
+            path: "/_matrix/client/r0/createRoom",
+            rate_limited: false,
+            requires_authentication: true,
+        }
+
+        request {
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub creation_content: Option<CreationContent>,
+            #[serde(skip_serializing_if = "Vec::is_empty")]
+            #[serde(default)]
+            pub invite: Vec<UserId>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub name: Option<String>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub preset: Option<RoomPreset>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub room_alias_name: Option<String>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub topic: Option<String>,
+            #[serde(skip_serializing_if = "Option::is_none")]
+            pub visibility: Option<String>, // TODO: should be an enum ["public", "private"]
+            // TODO: missing `invite_3pid`, `initial_state`
+        }
+
+        response {
+            pub room_id: RoomId,
+        }
     }
 
     /// Extra options to be added to the `m.room.create` event.
@@ -31,16 +45,6 @@ pub mod create_room {
         #[serde(rename="m.federate")]
         #[serde(skip_serializing_if = "Option::is_none")]
         pub federate: Option<bool>,
-    }
-
-    /// Details about this API endpoint.
-    #[derive(Clone, Copy, Debug)]
-    pub struct Endpoint;
-
-    /// The response type.
-    #[derive(Clone, Debug, Deserialize, Serialize)]
-    pub struct Response {
-        pub room_id: RoomId,
     }
 
     /// A convenience parameter for setting a few default state events.
@@ -55,40 +59,5 @@ pub mod create_room {
         /// Same as `PrivateChat`, but all initial invitees get the same power level as the creator.
         #[serde(rename="trusted_private_chat")]
         TrustedPrivateChat,
-    }
-
-    impl ::Endpoint for Endpoint {
-        type BodyParams = BodyParams;
-        type PathParams = ();
-        type QueryParams = ();
-        type Response = Response;
-
-        fn method() -> ::Method {
-            ::Method::Post
-        }
-
-        fn request_path(_params: Self::PathParams) -> String {
-            Self::router_path().to_string()
-        }
-
-        fn router_path() -> &'static str {
-            "/_matrix/client/r0/createRoom"
-        }
-
-        fn name() -> &'static str {
-            "create_room"
-        }
-
-        fn description() -> &'static str {
-            "Create a new room."
-        }
-
-        fn requires_authentication() -> bool {
-            true
-        }
-
-        fn rate_limited() -> bool {
-            false
-        }
     }
 }
