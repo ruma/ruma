59056808de84b315b59c8a5479778d7681ab41af
Jonas Platte <jplatte+git@posteo.de>
2019-11-02 14:15:49 +0100

    Remove #![deny(warnings)], #![warn(clippy::…)]

--- src/lib.rs
+++ src/lib.rs
@@ -13,26 +13,7 @@
 #![deny(
     missing_copy_implementations,
     missing_debug_implementations,
-    missing_docs,
-    warnings
-)]
-#![warn(
-    clippy::empty_line_after_outer_attr,
-    clippy::expl_impl_clone_on_copy,
-    clippy::if_not_else,
-    clippy::items_after_statements,
-    clippy::match_same_arms,
-    clippy::mem_forget,
-    clippy::missing_docs_in_private_items,
-    clippy::multiple_inherent_impl,
-    clippy::mut_mut,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::single_match_else,
-    clippy::unicode_not_nfc,
-    clippy::used_underscore_binding,
-    clippy::wrong_pub_self_convention,
-    clippy::wrong_self_convention
+    missing_docs
 )]
 // Since we support Rust 1.34.2, we can't apply this suggestion yet
 #![allow(clippy::use_self)]
