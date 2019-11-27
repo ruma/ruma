adevore/ruma-api

Add SendRecv trait + derive macro to allow receiving requests, sending responses

ruma-api-macros/src/api/response.rs
@@ -38,6 +38,11 @@ impl Response {
         self.fields.iter().any(|field| !field.is_header())
     }
 
+    /// Whether any field has a #[wrap_incoming] attribute.
+    pub fn uses_wrap_incoming(&self) -> bool {
+        self.fields.iter().any(|f| f.has_wrap_incoming_attr())
+    }
+
     /// Produces code for a response struct initializer.
     pub fn init_fields(&self) -> TokenStream {
         let fields = self.fields.iter().map(|response_field| match response_field {
@@ -83,10 +88,8 @@ impl Response {
     pub fn apply_header_fields(&self) -> TokenStream {
         let header_calls = self.fields.iter().filter_map(|response_field| {
             if let ResponseField::Header(ref field, ref header_name) = *response_field {
-                let field_name = field
-                    .ident
-                    .as_ref()
-                    .expect("expected field to have an identifier");
+                let field_name =
+                    field.ident.as_ref().expect("expected field to have an identifier");
                 let span = field.span();
 
                 Some(quote_spanned! {span=>
@@ -105,19 +108,14 @@ impl Response {
     /// Produces code to initialize the struct that will be used to create the response body.
     pub fn to_body(&self) -> TokenStream {
         if let Some(field) = self.newtype_body_field() {
-            let field_name = field
-                .ident
-                .as_ref()
-                .expect("expected field to have an identifier");
+            let field_name = field.ident.as_ref().expect("expected field to have an identifier");
             let span = field.span();
             quote_spanned!(span=> response.#field_name)
         } else {
             let fields = self.fields.iter().filter_map(|response_field| {
                 if let ResponseField::Body(ref field) = *response_field {
-                    let field_name = field
-                        .ident
-                        .as_ref()
-                        .expect("expected field to have an identifier");
+                    let field_name =
+                        field.ident.as_ref().expect("expected field to have an identifier");
                     let span = field.span();
 
                     Some(quote_spanned! {span=>
@@ -220,8 +218,8 @@ impl TryFrom<RawResponse> for Response {
             .collect::<syn::Result<Vec<_>>>()?;
 
         if newtype_body_field.is_some() && fields.iter().any(|f| f.is_body()) {
+            // TODO: highlight conflicting fields,
             return Err(syn::Error::new_spanned(
-                // TODO: raw,
                 raw.response_kw,
                 "Can't have both a newtype body field and regular body fields",
             ));
@@ -234,7 +232,8 @@ impl TryFrom<RawResponse> for Response {
 impl ToTokens for Response {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let response_struct_header = quote! {
-            #[derive(Debug, Clone)]
+            #[derive(Debug, Clone, ruma_api::SendRecv)]
+            #[incoming_no_deserialize]
             pub struct Response
         };
 
@@ -251,28 +250,51 @@ impl ToTokens for Response {
             }
         };
 
-        let response_body_struct = if let Some(field) = self.newtype_body_field() {
-            let ty = &field.ty;
-            let span = field.span();
+        let response_body_struct =
+            if let Some(body_field) = self.fields.iter().find(|f| f.is_newtype_body()) {
+                let field = body_field.field();
+                let ty = &field.ty;
+                let span = field.span();
+                let derive_deserialize = if body_field.has_wrap_incoming_attr() {
+                    TokenStream::new()
+                } else {
+                    quote!(ruma_api::exports::serde::Deserialize)
+                };
 
-            quote_spanned! {span=>
-                /// Data in the response body.
-                #[derive(Debug, ruma_api::exports::serde::Deserialize, ruma_api::exports::serde::Serialize)]
-                struct ResponseBody(#ty);
-            }
-        } else if self.has_body_fields() {
-            let fields = self.fields.iter().filter_map(ResponseField::as_body_field);
-
-            quote! {
-                /// Data in the response body.
-                #[derive(Debug, ruma_api::exports::serde::Deserialize, ruma_api::exports::serde::Serialize)]
-                struct ResponseBody {
-                    #(#fields),*
+                quote_spanned! {span=>
+                    /// Data in the response body.
+                    #[derive(
+                        Debug,
+                        ruma_api::SendRecv,
+                        ruma_api::exports::serde::Serialize,
+                        #derive_deserialize
+                    )]
+                    struct ResponseBody(#ty);
                 }
-            }
-        } else {
-            TokenStream::new()
-        };
+            } else if self.has_body_fields() {
+                let fields = self.fields.iter().filter(|f| f.is_body());
+                let derive_deserialize = if fields.clone().any(|f| f.has_wrap_incoming_attr()) {
+                    TokenStream::new()
+                } else {
+                    quote!(ruma_api::exports::serde::Deserialize)
+                };
+                let fields = fields.map(ResponseField::field);
+
+                quote! {
+                    /// Data in the response body.
+                    #[derive(
+                        Debug,
+                        ruma_api::SendRecv,
+                        ruma_api::exports::serde::Serialize,
+                        #derive_deserialize
+                    )]
+                    struct ResponseBody {
+                        #(#fields),*
+                    }
+                }
+            } else {
+                TokenStream::new()
+            };
 
         let response = quote! {
             #response_struct_header
@@ -317,6 +339,11 @@ impl ResponseField {
         }
     }
 
+    /// Whether or not this response field is a newtype body kind.
+    fn is_newtype_body(&self) -> bool {
+        self.as_body_field().is_some()
+    }
+
     /// Return the contained field if this response field is a body kind.
     fn as_body_field(&self) -> Option<&Field> {
         match self {
@@ -331,6 +358,13 @@ impl ResponseField {
             ResponseField::NewtypeBody(field) => Some(field),
             _ => None,
         }
+    }
+
+    /// Whether or not the reponse field has a #[wrap_incoming] attribute.
+    fn has_wrap_incoming_attr(&self) -> bool {
+        self.field().attrs.iter().any(|attr| {
+            attr.path.segments.len() == 1 && attr.path.segments[0].ident == "wrap_incoming"
+        })
     }
 }
 

ruma-api-macros/src/send_recv.rs
@@ -1,0 +1,192 @@
+use std::mem;
+
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{quote, ToTokens};
+use syn::{
+    parse_quote, punctuated::Pair, spanned::Spanned, Attribute, Data, DeriveInput, Fields,
+    GenericArgument, Path, PathArguments, Type, TypePath,
+};
+
+mod wrap_incoming;
+
+use wrap_incoming::Meta;
+
+pub fn expand_send_recv(input: DeriveInput) -> syn::Result<TokenStream> {
+    let derive_deserialize = if no_deserialize_in_attrs(&input.attrs) {
+        TokenStream::new()
+    } else {
+        quote!(#[derive(ruma_api::exports::serde::Deserialize)])
+    };
+
+    let mut fields: Vec<_> = match input.data {
+        Data::Enum(_) | Data::Union(_) => {
+            panic!("#[derive(SendRecv)] is only supported for structs")
+        }
+        Data::Struct(s) => match s.fields {
+            Fields::Named(fs) => fs.named.into_pairs().map(Pair::into_value).collect(),
+            Fields::Unnamed(fs) => fs.unnamed.into_pairs().map(Pair::into_value).collect(),
+            Fields::Unit => return Ok(impl_send_recv_incoming_self(input.ident)),
+        },
+    };
+
+    let mut any_attribute = false;
+
+    for field in &mut fields {
+        let mut field_meta = None;
+
+        let mut remaining_attrs = Vec::new();
+        for attr in mem::replace(&mut field.attrs, Vec::new()) {
+            if let Some(meta) = Meta::from_attribute(&attr)? {
+                if field_meta.is_some() {
+                    return Err(syn::Error::new_spanned(
+                        attr,
+                        "duplicate #[wrap_incoming] attribute",
+                    ));
+                }
+                field_meta = Some(meta);
+                any_attribute = true;
+            } else {
+                remaining_attrs.push(attr);
+            }
+        }
+        field.attrs = remaining_attrs;
+
+        if let Some(attr) = field_meta {
+            if let Some(type_to_wrap) = attr.type_to_wrap {
+                wrap_generic_arg(&type_to_wrap, &mut field.ty, attr.wrapper_type.as_ref())?;
+            } else {
+                wrap_ty(&mut field.ty, attr.wrapper_type)?;
+            }
+        }
+    }
+
+    if !any_attribute {
+        return Ok(impl_send_recv_incoming_self(input.ident));
+    }
+
+    let vis = input.vis;
+    let doc = format!("\"Incoming\" variant of [{ty}](struct.{ty}.html).", ty = input.ident);
+    let original_ident = input.ident;
+    let incoming_ident = Ident::new(&format!("Incoming{}", original_ident), Span::call_site());
+
+    Ok(quote! {
+        #[doc = #doc]
+        #derive_deserialize
+        #vis struct #incoming_ident {
+            #(#fields,)*
+        }
+
+        impl ruma_api::SendRecv for #original_ident {
+            type Incoming = #incoming_ident;
+        }
+    })
+}
+
+fn no_deserialize_in_attrs(attrs: &[Attribute]) -> bool {
+    for attr in attrs {
+        match &attr.path {
+            Path { leading_colon: None, segments }
+                if segments.len() == 1 && segments[0].ident == "incoming_no_deserialize" =>
+            {
+                return true
+            }
+            _ => {}
+        }
+    }
+
+    false
+}
+
+fn impl_send_recv_incoming_self(ident: Ident) -> TokenStream {
+    quote! {
+        impl ruma_api::SendRecv for #ident {
+            type Incoming = Self;
+        }
+    }
+}
+
+fn wrap_ty(ty: &mut Type, path: Option<Path>) -> syn::Result<()> {
+    if let Some(wrap_ty) = path {
+        *ty = parse_quote!(#wrap_ty<#ty>);
+    } else {
+        match ty {
+            Type::Path(TypePath { path, .. }) => {
+                let ty_ident = &mut path.segments.last_mut().unwrap().ident;
+                let ident = Ident::new(&format!("Incoming{}", ty_ident), Span::call_site());
+                *ty_ident = parse_quote!(#ident);
+            }
+            _ => return Err(syn::Error::new_spanned(ty, "Can't wrap this type")),
+        }
+    }
+
+    Ok(())
+}
+
+fn wrap_generic_arg(type_to_wrap: &Type, of: &mut Type, with: Option<&Path>) -> syn::Result<()> {
+    let mut span = None;
+    wrap_generic_arg_impl(type_to_wrap, of, with, &mut span)?;
+
+    if span.is_some() {
+        Ok(())
+    } else {
+        Err(syn::Error::new_spanned(
+            of,
+            format!(
+                "Couldn't find generic argument `{}` in this type",
+                type_to_wrap.to_token_stream()
+            ),
+        ))
+    }
+}
+
+fn wrap_generic_arg_impl(
+    type_to_wrap: &Type,
+    ty: &mut Type,
+    with: Option<&Path>,
+    span: &mut Option<Span>,
+) -> syn::Result<()> {
+    if ty == type_to_wrap {
+        if let Some(s) = span {
+            let mut error = syn::Error::new(
+                *s,
+                format!(
+                    "`{}` found multiple times, this is not currently supported",
+                    type_to_wrap.to_token_stream()
+                ),
+            );
+            error.combine(syn::Error::new_spanned(ty, "second occurrence"));
+            return Err(error);
+        }
+
+        *span = Some(ty.span());
+
+        if let Some(wrapper_type) = with {
+            *ty = parse_quote!(#wrapper_type<#ty>);
+        } else if let Type::Path(TypePath { path, .. }) = ty {
+            let ty_ident = &mut path.segments.last_mut().unwrap().ident;
+            let ident = Ident::new(&format!("Incoming{}", ty_ident), Span::call_site());
+            *ty_ident = parse_quote!(#ident);
+        } else {
+            return Err(syn::Error::new_spanned(ty, "Can't wrap this type"));
+        }
+    }
+
+    // TODO: Support things like array types?
+    let ty_path = match ty {
+        Type::Path(TypePath { path, .. }) => path,
+        _ => return Ok(()),
+    };
+
+    let args = match &mut ty_path.segments.last_mut().unwrap().arguments {
+        PathArguments::AngleBracketed(ab) => &mut ab.args,
+        _ => return Ok(()),
+    };
+
+    for arg in args.iter_mut() {
+        if let GenericArgument::Type(t) = arg {
+            wrap_generic_arg_impl(type_to_wrap, t, with, span)?;
+        }
+    }
+
+    Ok(())
+}

ruma/ruma

ruma-api-macros/src/api/response.rs
@@ -38,6 +38,11 @@ impl Response {
         self.fields.iter().any(|field| !field.is_header())
     }
 
+    /// Whether any field has a #[wrap_incoming] attribute.
+    pub fn uses_wrap_incoming(&self) -> bool {
+        self.fields.iter().any(|f| f.has_wrap_incoming_attr())
+    }
+
     /// Produces code for a response struct initializer.
     pub fn init_fields(&self) -> TokenStream {
         let fields = self.fields.iter().map(|response_field| match response_field {
@@ -83,10 +88,8 @@ impl Response {
     pub fn apply_header_fields(&self) -> TokenStream {
         let header_calls = self.fields.iter().filter_map(|response_field| {
             if let ResponseField::Header(ref field, ref header_name) = *response_field {
-                let field_name = field
-                    .ident
-                    .as_ref()
-                    .expect("expected field to have an identifier");
+                let field_name =
+                    field.ident.as_ref().expect("expected field to have an identifier");
                 let span = field.span();
 
                 Some(quote_spanned! {span=>
@@ -105,19 +108,14 @@ impl Response {
     /// Produces code to initialize the struct that will be used to create the response body.
     pub fn to_body(&self) -> TokenStream {
         if let Some(field) = self.newtype_body_field() {
-            let field_name = field
-                .ident
-                .as_ref()
-                .expect("expected field to have an identifier");
+            let field_name = field.ident.as_ref().expect("expected field to have an identifier");
             let span = field.span();
             quote_spanned!(span=> response.#field_name)
         } else {
             let fields = self.fields.iter().filter_map(|response_field| {
                 if let ResponseField::Body(ref field) = *response_field {
-                    let field_name = field
-                        .ident
-                        .as_ref()
-                        .expect("expected field to have an identifier");
+                    let field_name =
+                        field.ident.as_ref().expect("expected field to have an identifier");
                     let span = field.span();
 
                     Some(quote_spanned! {span=>
@@ -220,8 +218,8 @@ impl TryFrom<RawResponse> for Response {
             .collect::<syn::Result<Vec<_>>>()?;
 
         if newtype_body_field.is_some() && fields.iter().any(|f| f.is_body()) {
+            // TODO: highlight conflicting fields,
             return Err(syn::Error::new_spanned(
-                // TODO: raw,
                 raw.response_kw,
                 "Can't have both a newtype body field and regular body fields",
             ));
@@ -234,7 +232,8 @@ impl TryFrom<RawResponse> for Response {
 impl ToTokens for Response {
     fn to_tokens(&self, tokens: &mut TokenStream) {
         let response_struct_header = quote! {
-            #[derive(Debug, Clone)]
+            #[derive(Debug, Clone, ruma_api::SendRecv)]
+            #[incoming_no_deserialize]
             pub struct Response
         };
 
@@ -251,28 +250,51 @@ impl ToTokens for Response {
             }
         };
 
-        let response_body_struct = if let Some(field) = self.newtype_body_field() {
-            let ty = &field.ty;
-            let span = field.span();
+        let response_body_struct =
+            if let Some(body_field) = self.fields.iter().find(|f| f.is_newtype_body()) {
+                let field = body_field.field();
+                let ty = &field.ty;
+                let span = field.span();
+                let derive_deserialize = if body_field.has_wrap_incoming_attr() {
+                    TokenStream::new()
+                } else {
+                    quote!(ruma_api::exports::serde::Deserialize)
+                };
 
-            quote_spanned! {span=>
-                /// Data in the response body.
-                #[derive(Debug, ruma_api::exports::serde::Deserialize, ruma_api::exports::serde::Serialize)]
-                struct ResponseBody(#ty);
-            }
-        } else if self.has_body_fields() {
-            let fields = self.fields.iter().filter_map(ResponseField::as_body_field);
-
-            quote! {
-                /// Data in the response body.
-                #[derive(Debug, ruma_api::exports::serde::Deserialize, ruma_api::exports::serde::Serialize)]
-                struct ResponseBody {
-                    #(#fields),*
+                quote_spanned! {span=>
+                    /// Data in the response body.
+                    #[derive(
+                        Debug,
+                        ruma_api::SendRecv,
+                        ruma_api::exports::serde::Serialize,
+                        #derive_deserialize
+                    )]
+                    struct ResponseBody(#ty);
                 }
-            }
-        } else {
-            TokenStream::new()
-        };
+            } else if self.has_body_fields() {
+                let fields = self.fields.iter().filter(|f| f.is_body());
+                let derive_deserialize = if fields.clone().any(|f| f.has_wrap_incoming_attr()) {
+                    TokenStream::new()
+                } else {
+                    quote!(ruma_api::exports::serde::Deserialize)
+                };
+                let fields = fields.map(ResponseField::field);
+
+                quote! {
+                    /// Data in the response body.
+                    #[derive(
+                        Debug,
+                        ruma_api::SendRecv,
+                        ruma_api::exports::serde::Serialize,
+                        #derive_deserialize
+                    )]
+                    struct ResponseBody {
+                        #(#fields),*
+                    }
+                }
+            } else {
+                TokenStream::new()
+            };
 
         let response = quote! {
             #response_struct_header
@@ -317,6 +339,11 @@ impl ResponseField {
         }
     }
 
+    /// Whether or not this response field is a newtype body kind.
+    fn is_newtype_body(&self) -> bool {
+        self.as_newtype_body_field().is_some()
+    }
+
     /// Return the contained field if this response field is a body kind.
     fn as_body_field(&self) -> Option<&Field> {
         match self {
@@ -331,6 +358,13 @@ impl ResponseField {
             ResponseField::NewtypeBody(field) => Some(field),
             _ => None,
         }
+    }
+
+    /// Whether or not the reponse field has a #[wrap_incoming] attribute.
+    fn has_wrap_incoming_attr(&self) -> bool {
+        self.field().attrs.iter().any(|attr| {
+            attr.path.segments.len() == 1 && attr.path.segments[0].ident == "wrap_incoming"
+        })
     }
 }
 

ruma-api-macros/src/send_recv.rs
@@ -1,0 +1,195 @@
+use std::mem;
+
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::{quote, ToTokens};
+use syn::{
+    parse_quote, punctuated::Pair, spanned::Spanned, Attribute, Data, DeriveInput, Fields,
+    GenericArgument, Path, PathArguments, Type, TypePath,
+};
+
+mod wrap_incoming;
+
+use wrap_incoming::Meta;
+
+pub fn expand_send_recv(input: DeriveInput) -> syn::Result<TokenStream> {
+    let derive_deserialize = if no_deserialize_in_attrs(&input.attrs) {
+        TokenStream::new()
+    } else {
+        quote!(#[derive(ruma_api::exports::serde::Deserialize)])
+    };
+
+    let mut fields: Vec<_> = match input.data {
+        Data::Enum(_) | Data::Union(_) => {
+            panic!("#[derive(SendRecv)] is only supported for structs")
+        }
+        Data::Struct(s) => match s.fields {
+            Fields::Named(fs) => fs.named.into_pairs().map(Pair::into_value).collect(),
+            Fields::Unnamed(fs) => fs.unnamed.into_pairs().map(Pair::into_value).collect(),
+            Fields::Unit => return Ok(impl_send_recv_incoming_self(input.ident)),
+        },
+    };
+
+    let mut any_attribute = false;
+
+    for field in &mut fields {
+        let mut field_meta = None;
+
+        let mut remaining_attrs = Vec::new();
+        for attr in mem::replace(&mut field.attrs, Vec::new()) {
+            if let Some(meta) = Meta::from_attribute(&attr)? {
+                if field_meta.is_some() {
+                    return Err(syn::Error::new_spanned(
+                        attr,
+                        "duplicate #[wrap_incoming] attribute",
+                    ));
+                }
+                field_meta = Some(meta);
+                any_attribute = true;
+            } else {
+                remaining_attrs.push(attr);
+            }
+        }
+        field.attrs = remaining_attrs;
+
+        if let Some(attr) = field_meta {
+            if let Some(type_to_wrap) = attr.type_to_wrap {
+                wrap_generic_arg(&type_to_wrap, &mut field.ty, attr.wrapper_type.as_ref())?;
+            } else {
+                wrap_ty(&mut field.ty, attr.wrapper_type)?;
+            }
+        }
+    }
+
+    if !any_attribute {
+        return Ok(impl_send_recv_incoming_self(input.ident));
+    }
+
+    let vis = input.vis;
+    let doc = format!("\"Incoming\" variant of [{ty}](struct.{ty}.html).", ty = input.ident);
+    let original_ident = input.ident;
+    let incoming_ident = Ident::new(&format!("Incoming{}", original_ident), Span::call_site());
+
+    Ok(quote! {
+        #[doc = #doc]
+        #derive_deserialize
+        #vis struct #incoming_ident {
+            #(#fields,)*
+        }
+
+        impl ruma_api::SendRecv for #original_ident {
+            type Incoming = #incoming_ident;
+        }
+    })
+}
+
+fn no_deserialize_in_attrs(attrs: &[Attribute]) -> bool {
+    for attr in attrs {
+        match &attr.path {
+            Path { leading_colon: None, segments }
+                if segments.len() == 1 && segments[0].ident == "incoming_no_deserialize" =>
+            {
+                return true
+            }
+            _ => {}
+        }
+    }
+
+    false
+}
+
+fn impl_send_recv_incoming_self(ident: Ident) -> TokenStream {
+    quote! {
+        impl ruma_api::SendRecv for #ident {
+            type Incoming = Self;
+        }
+    }
+}
+
+fn wrap_ty(ty: &mut Type, path: Option<Path>) -> syn::Result<()> {
+    if let Some(wrap_ty) = path {
+        *ty = parse_quote!(#wrap_ty<#ty>);
+    } else {
+        match ty {
+            Type::Path(TypePath { path, .. }) => {
+                let ty_ident = &mut path.segments.last_mut().unwrap().ident;
+                let ident = Ident::new(&format!("Incoming{}", ty_ident), Span::call_site());
+                *ty_ident = parse_quote!(#ident);
+            }
+            _ => return Err(syn::Error::new_spanned(ty, "Can't wrap this type")),
+        }
+    }
+
+    Ok(())
+}
+
+fn wrap_generic_arg(type_to_wrap: &Type, of: &mut Type, with: Option<&Path>) -> syn::Result<()> {
+    let mut span = None;
+    wrap_generic_arg_impl(type_to_wrap, of, with, &mut span)?;
+
+    if span.is_some() {
+        Ok(())
+    } else {
+        Err(syn::Error::new_spanned(
+            of,
+            format!(
+                "Couldn't find generic argument `{}` in this type",
+                type_to_wrap.to_token_stream()
+            ),
+        ))
+    }
+}
+
+fn wrap_generic_arg_impl(
+    type_to_wrap: &Type,
+    of: &mut Type,
+    with: Option<&Path>,
+    span: &mut Option<Span>,
+) -> syn::Result<()> {
+    // TODO: Support things like array types?
+    let ty_path = match of {
+        Type::Path(TypePath { path, .. }) => path,
+        _ => return Ok(()),
+    };
+
+    let args = match &mut ty_path.segments.last_mut().unwrap().arguments {
+        PathArguments::AngleBracketed(ab) => &mut ab.args,
+        _ => return Ok(()),
+    };
+
+    for arg in args.iter_mut() {
+        let ty = match arg {
+            GenericArgument::Type(ty) => ty,
+            _ => continue,
+        };
+
+        if ty == type_to_wrap {
+            if let Some(s) = span {
+                let mut error = syn::Error::new(
+                    *s,
+                    format!(
+                        "`{}` found multiple times, this is not currently supported",
+                        type_to_wrap.to_token_stream()
+                    ),
+                );
+                error.combine(syn::Error::new_spanned(ty, "second occurrence"));
+                return Err(error);
+            }
+
+            *span = Some(ty.span());
+
+            if let Some(wrapper_type) = with {
+                *ty = parse_quote!(#wrapper_type<#ty>);
+            } else if let Type::Path(TypePath { path, .. }) = ty {
+                let ty_ident = &mut path.segments.last_mut().unwrap().ident;
+                let ident = Ident::new(&format!("Incoming{}", ty_ident), Span::call_site());
+                *ty_ident = parse_quote!(#ident);
+            } else {
+                return Err(syn::Error::new_spanned(ty, "Can't wrap this type"));
+            }
+        } else {
+            wrap_generic_arg_impl(type_to_wrap, ty, with, span)?;
+        }
+    }
+
+    Ok(())
+}