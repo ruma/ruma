6a844625fc5e31e882013f30d6fdd6562f13aafb
Jonas Platte <jplatte+git@posteo.de>
2020-06-04 11:31:59 +0200

    Clean up macro crate attributes

--- ruma-events-macros/src/lib.rs
+++ ruma-events-macros/src/lib.rs
@@ -1,33 +1,12 @@
 //! Crate `ruma_events_macros` provides a procedural macro for generating
 //! [ruma-events](https://github.com/ruma/ruma-events) events.
 //!
-//! See the documentation for the `ruma_event!` macro for usage details.
+//! See the documentation for the invidiual macros for usage details.
 #![deny(
     missing_copy_implementations,
     missing_debug_implementations,
-    // missing_docs, # Uncomment when https://github.com/rust-lang/rust/pull/60562 is released.
+    missing_docs
 )]
-#![warn(
-    clippy::empty_line_after_outer_attr,
-    clippy::expl_impl_clone_on_copy,
-    clippy::if_not_else,
-    clippy::items_after_statements,
-    clippy::match_same_arms,
-    clippy::mem_forget,
-    clippy::multiple_inherent_impl,
-    clippy::mut_mut,
-    clippy::needless_borrow,
-    clippy::needless_continue,
-    clippy::single_match_else,
-    clippy::unicode_not_nfc,
-    clippy::use_self,
-    clippy::used_underscore_binding,
-    clippy::wrong_pub_self_convention,
-    clippy::wrong_self_convention
-)]
-// Since we support Rust 1.36.0, we can't apply this suggestion yet
-#![allow(clippy::use_self)]
-#![recursion_limit = "128"]
 
 extern crate proc_macro;
 
