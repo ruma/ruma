6a8bebbef27b1b175b0d47a019d410390916874c
Guillem Nieto <gnieto.talo@gmail.com>
2020-07-05 11:56:21 +0200

    Apply

--- ruma-client-api/src/r0/tag/get_tags.rs
+++ ruma-client-api/src/r0/tag/get_tags.rs
@@ -1,7 +1,7 @@
 //! [GET /_matrix/client/r0/user/{userId}/rooms/{roomId}/tags](https://matrix.org/docs/spec/client_server/r0.6.0#get-matrix-client-r0-user-userid-rooms-roomid-tags)
 
 use ruma_api::ruma_api;
-use ruma_events::{tag::Tags};
+use ruma_events::tag::Tags;
 use ruma_identifiers::{RoomId, UserId};
 
 ruma_api! {
@@ -37,25 +37,20 @@ mod tests {
     use serde_json::json;
 
     use super::Response;
-    use ruma_events::tag::{Tags, TagInfo};
+    use ruma_events::tag::{TagInfo, Tags};
     use std::convert::TryFrom;
 
     #[test]
     fn test_serializing_get_tags_response() {
         let mut tags = Tags::new();
-        tags.insert("m.favourite".to_string(), TagInfo {
-            order: Some(0.25),
-        });
-        tags.insert("u.user_tag".to_string(), TagInfo {
-            order: Some(0.11),
-        });
-        let response = Response {
-            tags,
-        };
+        tags.insert("m.favourite".to_string(), TagInfo { order: Some(0.25) });
+        tags.insert("u.user_tag".to_string(), TagInfo { order: Some(0.11) });
+        let response = Response { tags };
 
         let http_response = http::Response::<Vec<u8>>::try_from(response).unwrap();
 
-        let json_response: serde_json::Value = serde_json::from_slice(http_response.body()).unwrap();
+        let json_response: serde_json::Value =
+            serde_json::from_slice(http_response.body()).unwrap();
         assert_eq!(
             json_response,
             json!({
