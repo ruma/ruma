60238bbb8568bb85ff80b6d490f23ab868d53190
Jonas Platte <jplatte+git@posteo.de>
2020-07-15 02:02:14 +0200

    Fix doc_concat macro on older versions of rustc

--- ruma-identifiers/src/macros.rs
+++ ruma-identifiers/src/macros.rs
@@ -2,7 +2,7 @@
 /// This allows documentation to be dynamically generated based on input.
 /// Necessary to work around https://github.com/rust-lang/rust/issues/52607.
 macro_rules! doc_concat {
-    ( $( #[doc = $doc:expr] $thing:item )* ) => ( $( #[doc = $doc] $thing )* );
+    ( $( #[doc = $doc:expr] $( $thing:tt )* )* ) => ( $( #[doc = $doc] $( $thing )* )* );
 }
 
 macro_rules! common_impls {
