34e89f4d1199d300f677107cf7bc8453916ea9bd
Jonas Platte <jplatte+git@posteo.de>
2020-07-17 01:20:30 +0200

    Update EncryptionEventContent docs

--- ruma-events/src/room/encryption.rs
+++ ruma-events/src/room/encryption.rs
@@ -20,13 +20,13 @@ pub struct EncryptionEventContent {
 
     /// How long the session should be used before changing it.
     ///
-    /// 604800000 (a week) is the recommended default.
+    /// `uint!(604800000)` (a week) is the recommended default.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub rotation_period_ms: Option<UInt>,
 
     /// How many messages should be sent before changing the session.
     ///
-    /// 100 is the recommended default.
+    /// `uint!(100)` is the recommended default.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub rotation_period_msgs: Option<UInt>,
 }
