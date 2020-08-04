a589e92144e1af635ec3fd224265193e45dcdc95
Jonas Platte <jplatte+git@posteo.de>
2020-08-05 00:32:05 +0200

    Move, update identifier macro tests

---
+++ ruma-identifiers/tests/ui/02-invalid-id-macros.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _ = ruma_identifiers::device_key_id!("ed2519:JLAFKJWSCS");
+    let _ = ruma_identifiers::event_id!("39hvsi03hlne:example.com");
+    let _ = ruma_identifiers::event_id!("acR1l0raoZnm60CBwAVgqbZqoO/mYU81xysh1u7XcJk");
+    let _ = ruma_identifiers::room_alias_id!("alias:server.tld");
+    let _ = ruma_identifiers::room_id!("1234567890:matrix.org");
+    let _ = ruma_identifiers::room_version_id!("");
+    let _ = ruma_identifiers::server_key_id!("ed219:Abc_1");
+    let _ = ruma_identifiers::server_name!("");
+    let _ = ruma_identifiers::user_id!("user:ruma.io");
+}
