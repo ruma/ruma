e7f7c3bb9d3d2fcd9207e9c52fe5e1c2637aef68
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-08-04 20:02:20 -0400

    Add lifetime params to Request/Response when needed

--- ruma-api/tests/outgoing.rs
+++ ruma-api/tests/outgoing.rs
@@ -54,24 +54,24 @@ ruma_api! {
     }
 }
 
-// #[derive(Outgoing)]
-// #[incoming_no_deserialize]
-// pub struct Request<'a, T> {
-//     pub abc: &'a str,
-//     pub thing: Thing<'a, T>,
-//     pub device_id: &'a ::ruma_identifiers::DeviceId,
-//     pub user_id: &'a UserId,
-//     pub bytes: &'a [u8],
-//     pub recursive: &'a [Thing<'a, T>],
-//     pub option: Option<&'a [u8]>,
-// }
+#[derive(Outgoing)]
+#[incoming_no_deserialize]
+pub struct FakeRequest<'a, T> {
+    pub abc: &'a str,
+    pub thing: Thing<'a, T>,
+    pub device_id: &'a ::ruma_identifiers::DeviceId,
+    pub user_id: &'a UserId,
+    pub bytes: &'a [u8],
+    pub recursive: &'a [Thing<'a, T>],
+    pub option: Option<&'a [u8]>,
+}
 
-// #[derive(Outgoing)]
-// #[incoming_no_deserialize]
-// pub enum EnumThing<'a, T> {
-//     Abc(&'a str),
-//     Stuff(Thing<'a, T>),
-//     Boxy(&'a ::ruma_identifiers::DeviceId),
-//     Other(Option<&'a str>),
-//     StructVar { stuff: &'a str, more: &'a ::ruma_identifiers::ServerName },
-// }
+#[derive(Outgoing)]
+#[incoming_no_deserialize]
+pub enum EnumThing<'a, T> {
+    Abc(&'a str),
+    Stuff(Thing<'a, T>),
+    Boxy(&'a ::ruma_identifiers::DeviceId),
+    Other(Option<&'a str>),
+    StructVar { stuff: &'a str, more: &'a ::ruma_identifiers::ServerName },
+}
