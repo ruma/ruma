bffc702fddd89ebf6cf277615aea703c9ca2bfe0
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-08-07 14:10:40 -0400

    fixup! Convert alias mod and get_public_rooms to use Outgoing trait

--- ruma-client-api/src/r0/account/add_3pid.rs
+++ ruma-client-api/src/r0/account/add_3pid.rs
@@ -17,7 +17,7 @@ ruma_api! {
     request: {
         /// Additional information for the User-Interactive Authentication API.
         #[serde(skip_serializing_if = "Option::is_none")]
-        pub auth: Option<&'a AuthData>,
+        pub auth: Option<AuthData>,
 
         /// Client-generated secret string used to protect this session.
         pub client_secret: &'a str,
