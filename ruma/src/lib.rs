3dadd0a772403346241ac863cdad099184b2022b
Florian Jacob <projects+git@florianjacob.de>
2020-08-13 09:41:12 +0200

    ruma: Export serde helpers for custom api creation

--- ruma/src/lib.rs
+++ ruma/src/lib.rs
@@ -16,6 +16,8 @@
 pub use ruma_common::*;
 #[doc(inline)]
 pub use ruma_identifiers as identifiers;
+#[doc(inline)]
+pub use ruma_serde as serde;
 
 pub use ruma_identifiers::{
     device_id, device_key_id, event_id, room_alias_id, room_id, room_version_id, server_key_id,
