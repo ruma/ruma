3d8ab780ac90fb21d1bba1a9e4eedd401f59fdba
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-08-14 09:26:42 -0400

    Fix name field and spec link

--- ruma-client-api/src/r0/room/aliases.rs
+++ ruma-client-api/src/r0/room/aliases.rs
@@ -1,4 +1,4 @@
-//! [GET /_matrix/client/r0/rooms/{roomId}/aliases](https://matrix.org/docs/spec/client_server/latest#get-matrix-client-r0-rooms-roomid-aliases)
+//! [GET /_matrix/client/r0/rooms/{roomId}/aliases](https://matrix.org/docs/spec/client_server/r0.6.1#get-matrix-client-r0-rooms-roomid-aliases)
 
 use ruma_api::ruma_api;
 use ruma_identifiers::{RoomAliasId, RoomId};
@@ -7,7 +7,7 @@ ruma_api! {
     metadata: {
         description: "Get a list of aliases maintained by the local server for the given room.",
         method: GET,
-        name: "create_alias",
+        name: "aliases",
         path: "/_matrix/client/r0/rooms/:room_id/aliases",
         rate_limited: true,
         requires_authentication: true,
