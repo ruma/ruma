2e56f23dc4a90d21ee9bb73aecc2d2e146485691
Devin Ragotzy <devin.ragotzy@gmail.com>
2020-08-19 20:22:56 -0400

    Add failing optional_headers test

---
+++ ruma-api/tests/optional_headers.rs
@@ -0,0 +1,20 @@
+use ruma_api::ruma_api;
+
+ruma_api! {
+    metadata: {
+        description: "Does something.",
+        method: GET,
+        name: "no_fields",
+        path: "/_matrix/my/endpoint",
+        rate_limited: false,
+        requires_authentication: false,
+    }
+
+    request: {
+        #[ruma_api(header = LOCATION)]
+        location: Option<String>,
+    }
+    response: {
+        stuff: String,
+    }
+}
