db2f58032953ccb6d8ae712d64d713ebdf412598
poljar <poljar@termina.org.uk>
2020-10-22 16:21:27 +0200

    client-api: Make the CrossSigningKey serialization invertible

--- ruma-client-api/src/r0/keys.rs
+++ ruma-client-api/src/r0/keys.rs
@@ -49,7 +49,7 @@ pub struct CrossSigningKey {
     pub keys: BTreeMap<String, String>,
 
     /// Signatures of the key. Only optional for master key.
-    #[serde(skip_serializing_if = "BTreeMap::is_empty")]
+    #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
     pub signatures: BTreeMap<UserId, BTreeMap<String, String>>,
 }
 
