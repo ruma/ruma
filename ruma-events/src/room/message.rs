e659df73a13c38ff37fa903c4168a20e5fdbc151
Jonas Platte <jplatte+git@posteo.de>
2021-02-18 00:56:32 +0100

    Fix m.room.message serialization

--- ruma-events/src/room/message.rs
+++ ruma-events/src/room/message.rs
@@ -89,7 +89,7 @@ impl MessageEventContent {
 /// The content that is specific to each message type variant.
 #[derive(Clone, Debug, Serialize)]
 #[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
-#[serde(tag = "msgtype")]
+#[serde(untagged)]
 pub enum MessageType {
     /// An audio message.
     Audio(AudioMessageEventContent),
