92ee92ad7eb90b3c80abbd7eb116d886c79bf4fd
Kévin Commaille <76261501+zecakeh@users.noreply.github.com>
2021-03-15 12:15:43 +0100

    client-api: Fix sso_login query param serialization

--- ruma-client-api/src/r0/session/sso_login.rs
+++ ruma-client-api/src/r0/session/sso_login.rs
@@ -17,6 +17,7 @@ ruma_api! {
         /// URL to which the homeserver should return the user after completing
         /// authentication with the SSO identity provider.
         #[ruma_api(query)]
+        #[serde(rename = "redirectUrl")]
         pub redirect_url: &'a str,
     }
 
@@ -40,5 +41,24 @@ impl Response {
     /// Creates a new `Response` with the given SSO URL.
     pub fn new(location: String) -> Self {
         Self { location }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ruma_api::OutgoingRequest;
+
+    use super::Request;
+
+    #[test]
+    fn serialize_sso_login_request_uri() {
+        let req: http::Request<Vec<u8>> = Request { redirect_url: "https://example.com/sso" }
+            .try_into_http_request("https://homeserver.tld", None)
+            .unwrap();
+
+        assert_eq!(
+            req.uri().to_string(),
+            "https://homeserver.tld/_matrix/client/r0/login/sso/redirect?redirectUrl=https%3A%2F%2Fexample.com%2Fsso"
+        );
     }
 }
