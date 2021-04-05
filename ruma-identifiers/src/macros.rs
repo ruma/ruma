0dcbec5fa7760caf6873b8b19c4207788c6e0877
Jonas Platte <jplatte+git@posteo.de>
2021-04-05 21:41:54 +0200

    identifiers: Add more conversion methods and a trait impl

--- ruma-identifiers/src/macros.rs
+++ ruma-identifiers/src/macros.rs
@@ -92,11 +92,31 @@ macro_rules! common_impls {
                     self.full_id.as_bytes()
                 }
             }
+
+            doc_concat! {
+                #[doc = concat!("Converts this `", stringify!($id), "` into a `String`")]
+                pub fn into_string(self) -> String {
+                    self.full_id.into()
+                }
+            }
+
+            doc_concat! {
+                #[doc = concat!("Converts this `", stringify!($id), "` into a `Vec<u8>`")]
+                pub fn into_bytes(self) -> Vec<u8> {
+                    Box::<[u8]>::from(self.full_id).into()
+                }
+            }
         }
 
         impl ::std::convert::From<$id> for ::std::string::String {
             fn from(id: $id) -> Self {
-                id.full_id.into()
+                id.into_string()
+            }
+        }
+
+        impl ::std::convert::From<$id> for ::std::vec::Vec<u8> {
+            fn from(id: $id) -> Self {
+                id.into_bytes()
             }
         }
 
