0d617995b255c9e8ebfc14c3358c4dbd96e25e24
Jonas Platte <jplatte+git@posteo.de>
2021-04-05 11:43:35 +0200

    identity-service-api: Add crate-level docs and warn on missing docs

--- ruma-identity-service-api/src/lib.rs
+++ ruma-identity-service-api/src/lib.rs
@@ -1,5 +1,8 @@
 #![doc(html_favicon_url = "https://www.ruma.io/favicon.ico")]
 #![doc(html_logo_url = "https://www.ruma.io/images/logo.png")]
+//! (De)serializable types for the Matrix Identity Service API.
+
+#![warn(missing_docs)]
 
 pub mod authentication;
 pub mod keys;
