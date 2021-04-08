ddaaca1ec432284ad44f35356b73ec77d62cb3af
Jonas Platte <jplatte+git@posteo.de>
2021-04-08 14:05:00 +0200

    identifiers: Fix line wrapping

--- ruma-identifiers/src/opaque_ids.rs
+++ ruma-identifiers/src/opaque_ids.rs
@@ -23,8 +23,8 @@ impl DeviceId {
 opaque_identifier! {
     /// A Matrix key identifier.
     ///
-    /// Key identifiers in Matrix are opaque character sequences of `[a-zA-Z_]`. This type is provided
-    /// simply for its semantic value.
+    /// Key identifiers in Matrix are opaque character sequences of `[a-zA-Z_]`. This type is
+    /// provided simply for its semantic value.
     pub type KeyName;
 }
 
