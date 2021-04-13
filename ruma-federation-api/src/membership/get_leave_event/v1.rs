6394609feb4af5c43b840fab85b824b13cebb156
Timo Kösters <timo@koesters.xyz>
2021-04-13 15:10:01 +0200

    federation-api: Clean up create_leave_event and get_leave_event

--- ruma-federation-api/src/membership/get_leave_event/v1.rs
+++ ruma-federation-api/src/membership/get_leave_event/v1.rs
@@ -1,12 +1,9 @@
 //! [GET /_matrix/federation/v1/make_leave/{roomId}/{userId}](https://matrix.org/docs/spec/server_server/r0.1.4#get-matrix-federation-v1-make-leave-roomid-userid)
 
-use std::time::SystemTime;
-
 use ruma_api::ruma_api;
-use ruma_events::{room::member::MemberEventContent, EventType};
-use ruma_identifiers::{RoomId, RoomVersionId, ServerNameBox, UserId};
+use ruma_events::pdu::Pdu;
+use ruma_identifiers::{RoomId, RoomVersionId, UserId};
 use ruma_serde::Raw;
-use serde::{Deserialize, Serialize};
 
 ruma_api! {
     metadata: {
@@ -35,7 +32,7 @@ ruma_api! {
 
         /// An unsigned template event. Note that events have a different format depending on the
         /// room version - check the room version specification for precise event formats.
-        pub event: EventTemplate,
+        pub event: Raw<Pdu>,
     }
 }
 
@@ -52,48 +49,7 @@ impl Response {
     /// Creates a new `Response` with:
     /// * the version of the room where the server is trying to leave.
     /// * an unsigned template event.
-    pub fn new(room_version: Option<RoomVersionId>, event: EventTemplate) -> Self {
+    pub fn new(room_version: Option<RoomVersionId>, event: Raw<Pdu>) -> Self {
         Self { room_version, event }
-    }
-}
-
-/// An unsigned template event. Note that events have a different format depending on the room
-/// version - check the room version specification for precise event formats.
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
-pub struct EventTemplate {
-    /// The user ID of the leaving member.
-    pub sender: UserId,
-
-    /// The name of the resident homeserver.
-    pub origin: ServerNameBox,
-
-    /// A timestamp added by the resident homeserver.
-    #[serde(with = "ruma_serde::time::ms_since_unix_epoch")]
-    pub origin_server_ts: SystemTime,
-
-    /// The value `m.room.member`.
-    #[serde(rename = "type")]
-    pub event_type: EventType,
-
-    /// The user ID of the leaving member.
-    pub state_key: String,
-
-    /// The content of the event.
-    pub content: Raw<MemberEventContent>,
-}
-
-impl EventTemplate {
-    /// Creates a new `EventTemplate` with the given sender, origin, timestamp, state key and
-    /// content.
-    pub fn new(
-        sender: UserId,
-        origin: ServerNameBox,
-        origin_server_ts: SystemTime,
-        event_type: EventType,
-        state_key: String,
-        content: Raw<MemberEventContent>,
-    ) -> Self {
-        Self { sender, origin, origin_server_ts, event_type, state_key, content }
     }
 }
