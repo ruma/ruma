7666c23053bce04c6aad4345e5330396822ca8c0
Abhik Jain <abhikjain360@gmail.com>
2021-04-14 08:49:13 +0530

    add enum for 3pid lookup

--- ruma-identity-service-api/src/lookup.rs
+++ ruma-identity-service-api/src/lookup.rs
@@ -1,3 +1,49 @@
 //! Endpoints to look up Matrix IDs bound to 3PIDs.
 
+use ruma_serde_macros::{AsRefStr, DisplayAsRefStr, FromString};
+use serde::{Deserialize, Serialize};
+
 pub mod lookup_3pid;
+
+/// The algorithms that can be used to hash the identifiers used for lookup, as defined in the
+/// Matrix Spec.
+///
+/// This type can hold an arbitrary string. To check for algorithms that are not available as a
+/// documented variant here, use its string representation, obtained through `.as_str()`.
+#[derive(
+    Clone,
+    Debug,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    AsRefStr,
+    DisplayAsRefStr,
+    FromString,
+    Serialize,
+    Deserialize,
+)]
+#[non_exhaustive]
+#[ruma_enum(rename_all = "snake_case")]
+#[cfg_attr(feature = "serde", derive(DeserializeFromCowStr, SerializeAsRefStr))]
+pub enum IdentifierHashingAlgorithm {
+    /// The SHA-256 hashing algorithm.
+    Sha256,
+
+    /// No algorithm is used, and identifier strings are directly used for lookup.
+    None,
+
+    #[doc(hidden)]
+    _Custom(String),
+}
+
+#[cfg(test)]
+mod test {
+    use super::IdentifierHashingAlgorithm;
+
+    #[test]
+    fn parse_identifier_hashing_algorithm() {
+        assert_eq!(IdentifierHashingAlgorithm::from("sha256"), IdentifierHashingAlgorithm::Sha256);
+        assert_eq!(IdentifierHashingAlgorithm::from("none"), IdentifierHashingAlgorithm::None);
+    }
+}
--- ruma-identity-service-api/src/lookup/lookup_3pid/v2.rs
+++ ruma-identity-service-api/src/lookup/lookup_3pid/v2.rs
@@ -1,8 +1,11 @@
-//! [POST /_matrix/identity/v2/lookup](https://matrix.org/docs/spec/identity_service/latest#post-matrix-identity-v2-lookup)
+//! [POST /_matrix/identity/v2/lookup](https://matrix.org/docs/spec/identity_service/0.3.0#post-matrix-identity-v2-lookup)
 
 use std::collections::BTreeMap;
 
 use ruma_api::ruma_api;
+use ruma_identifiers::UserId;
+
+use crate::lookup::IdentifierHashingAlgorithm;
 
 ruma_api! {
     metadata: {
@@ -17,7 +20,7 @@ ruma_api! {
     request: {
         /// The algorithm the client is using to encode the `addresses`. This should be one of the
         /// available options from `/hash_details`.
-        pub algorithm: &'a str,
+        pub algorithm: IdentifierHashingAlgorithm,
 
         /// The pepper from `/hash_details`. This is required even when the `algorithm` does not
         /// make use of it.
@@ -26,19 +29,23 @@ ruma_api! {
         /// The addresses to look up. The format of the entries here depend on the `algorithm`
         /// used. Note that queries which have been incorrectly hashed or formatted will lead to no
         /// matches.
-        pub addresses: Vec<String>
+        pub addresses: &'a [String]
     }
 
     response: {
         /// Any applicable mappings of `addresses` to Matrix User IDs. Addresses which do not have
         /// associations will not be included, which can make this property be an empty object.
-        pub mappings: BTreeMap<String, String>,
+        pub mappings: BTreeMap<String, UserId>,
     }
 }
 
 impl<'a> Request<'a> {
     /// Create a `Request` with algorithm, pepper and addresses to loop up.
-    pub fn new(algorithm: &'a str, pepper: &'a str, addresses: Vec<String>) -> Self {
+    pub fn new(
+        algorithm: IdentifierHashingAlgorithm,
+        pepper: &'a str,
+        addresses: &'a [String],
+    ) -> Self {
         Self { algorithm, pepper, addresses }
     }
 }
@@ -46,7 +53,7 @@ impl<'a> Request<'a> {
 impl Response {
     /// Create a `Response` with the BTreeMap which map addresses from the request which were
     /// found to their corresponding User IDs.
-    pub fn new(mappings: BTreeMap<String, String>) -> Self {
+    pub fn new(mappings: BTreeMap<String, UserId>) -> Self {
         Self { mappings }
     }
 }
