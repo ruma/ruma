88c36ac02caab63d36ef2943f30ff470145dbb50
Jonas Platte <jplatte+git@posteo.de>
2021-04-27 01:16:33 +0200

    WIP

--- ruma-client/src/http_client.rs
+++ ruma-client/src/http_client.rs
@@ -3,6 +3,9 @@
 
 use async_trait::async_trait;
 use bytes::BufMut;
+use ruma_api::{OutgoingRequest, SendAccessToken};
+
+use crate::Error;
 
 #[cfg(feature = "hyper")]
 mod hyper;
@@ -18,7 +21,7 @@ pub use self::hyper::HyperRustls;
 #[async_trait]
 pub trait HttpClient {
     /// The type to use for `try_into_http_request`.
-    type RequestBody: Default + BufMut;
+    type RequestBody: Default + BufMut + Send;
 
     /// The type to use for `try_from_http_response`.
     type ResponseBody: AsRef<[u8]>;
@@ -38,3 +41,23 @@ pub trait DefaultConstructibleHttpClient: HttpClient {
     /// Creates a new HTTP client with default configuration.
     fn default() -> Self;
 }
+
+/// Convenience functionality on top of `HttpClient`.
+///
+/// If you want to build your own matrix client type instead of using `ruma_client::Client`, this
+/// trait should make that relatively easy.
+#[async_trait]
+pub trait HttpClientExt: HttpClient {
+    /// Send a strongly-typed matrix request to get back a strongly-typed response.
+    async fn send_request<Request: OutgoingRequest>(
+        &self,
+        homeserver_url: &str,
+        access_token: SendAccessToken<'_>,
+        request: Request,
+    ) -> Result<Request::IncomingResponse, Error<Self::Error, Request::EndpointError>> {
+        crate::send_request_with_url_params(self, homeserver_url, access_token, None, request).await
+    }
+}
+
+#[async_trait]
+impl<T: HttpClient> HttpClientExt for T {}
--- ruma-client/src/lib.rs
+++ ruma-client/src/lib.rs
@@ -101,12 +101,13 @@
 
 use std::{
     collections::BTreeMap,
+    future::Future,
     sync::{Arc, Mutex},
 };
 
 use assign::assign;
 use http::uri::Uri;
-use ruma_api::{AuthScheme, OutgoingRequest, SendAccessToken};
+use ruma_api::{OutgoingRequest, SendAccessToken};
 use ruma_serde::urlencoded;
 
 // "Undo" rename from `Cargo.toml` that only serves to make `hyper-rustls` available as a Cargo
@@ -132,7 +133,7 @@ pub struct Client<C>(Arc<ClientData<C>>);
 #[derive(Debug)]
 struct ClientData<C> {
     /// The URL of the homeserver to connect to.
-    homeserver_url: Uri,
+    homeserver_url: String,
 
     /// The underlying HTTP client.
     http_client: C,
@@ -147,7 +148,7 @@ impl<C> Client<C> {
     /// This allows the user to configure the details of HTTP as desired.
     pub fn with_http_client(
         http_client: C,
-        homeserver_url: Uri,
+        homeserver_url: String,
         access_token: Option<String>,
     ) -> Self {
         Self(Arc::new(ClientData {
@@ -167,7 +168,7 @@ impl<C> Client<C> {
 
 impl<C: DefaultConstructibleHttpClient> Client<C> {
     /// Creates a new client based on a default-constructed hyper HTTP client.
-    pub fn new(homeserver_url: Uri, access_token: Option<String>) -> Self {
+    pub fn new(homeserver_url: String, access_token: Option<String>) -> Self {
         Self(Arc::new(ClientData {
             homeserver_url,
             http_client: DefaultConstructibleHttpClient::default(),
@@ -182,44 +183,77 @@ impl<C: HttpClient> Client<C> {
         &self,
         request: Request,
     ) -> Result<Request::IncomingResponse, Error<C::Error, Request::EndpointError>> {
-        self.request_with_url_params(request, None).await
+        let access_token = self.access_token();
+        let send_access_token = match access_token.as_deref() {
+            Some(at) => SendAccessToken::IfRequired(at),
+            None => SendAccessToken::None,
+        };
+
+        send_request_with_url_params(
+            &self.0.http_client,
+            &self.0.homeserver_url,
+            send_access_token,
+            None,
+            request,
+        )
+        .await
     }
 
     /// Makes a request to a Matrix API endpoint including additional URL parameters.
     pub async fn request_with_url_params<Request: OutgoingRequest>(
         &self,
+        extra_params: BTreeMap<String, String>,
         request: Request,
-        extra_params: Option<BTreeMap<String, String>>,
     ) -> Result<Request::IncomingResponse, Error<C::Error, Request::EndpointError>> {
-        let client = self.0.clone();
-        let mut http_request = {
-            let lock;
-            let access_token = if Request::METADATA.authentication == AuthScheme::AccessToken {
-                lock = client.access_token.lock().unwrap();
-                if let Some(access_token) = &*lock {
-                    SendAccessToken::IfRequired(access_token.as_str())
-                } else {
-                    return Err(Error::AuthenticationRequired);
-                }
-            } else {
-                SendAccessToken::None
+        let access_token = self.access_token();
+        let send_access_token = match access_token.as_deref() {
+            Some(at) => SendAccessToken::IfRequired(at),
+            None => SendAccessToken::None,
+        };
+
+        send_request_with_url_params(
+            &self.0.http_client,
+            &self.0.homeserver_url,
+            send_access_token,
+            Some(extra_params),
+            request,
+        )
+        .await
+    }
+}
+
+fn send_request_with_url_params<'a, C, Request>(
+    http_client: &'a C,
+    homeserver_url: &str,
+    send_access_token: SendAccessToken<'_>,
+    extra_params: Option<BTreeMap<String, String>>,
+    request: Request,
+) -> impl Future<Output = Result<Request::IncomingResponse, Error<C::Error, Request::EndpointError>>>
+       + Send
+       + 'a
+where
+    C: HttpClient + ?Sized,
+    Request: OutgoingRequest,
+{
+    let res = request.try_into_http_request(homeserver_url, send_access_token);
+
+    async move {
+        let mut http_request = res?;
+
+        if let Some(extra_params) = extra_params {
+            let extra_params = urlencoded::to_string(extra_params).unwrap();
+            let uri = http_request.uri_mut();
+            let new_path_and_query = match uri.query() {
+                Some(params) => format!("{}?{}&{}", uri.path(), params, extra_params),
+                None => format!("{}?{}", uri.path(), extra_params),
             };
-
-            request.try_into_http_request(&client.homeserver_url.to_string(), access_token)?
-        };
-
-        let extra_params = urlencoded::to_string(extra_params).unwrap();
-        let uri = http_request.uri_mut();
-        let new_path_and_query = match uri.query() {
-            Some(params) => format!("{}?{}&{}", uri.path(), params, extra_params),
-            None => format!("{}?{}", uri.path(), extra_params),
-        };
-        *uri = Uri::from_parts(assign!(uri.clone().into_parts(), {
-            path_and_query: Some(new_path_and_query.parse()?),
-        }))?;
+            *uri = Uri::from_parts(assign!(uri.clone().into_parts(), {
+                path_and_query: Some(new_path_and_query.parse()?),
+            }))?;
+        }
 
         let http_response =
-            client.http_client.send_http_request(http_request).await.map_err(Error::Response)?;
+            http_client.send_http_request(http_request).await.map_err(Error::Response)?;
         Ok(ruma_api::IncomingResponse::try_from_http_response(http_response)?)
     }
 }
