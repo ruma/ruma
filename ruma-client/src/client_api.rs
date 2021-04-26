15c9e470c80844da8539c41689eaf9735f2d1a59
Jonas Platte <jplatte+git@posteo.de>
2021-04-26 22:42:07 +0200

    client: Remove session data other than the access token

--- ruma-client/src/client_api.rs
+++ ruma-client/src/client_api.rs
@@ -3,34 +3,32 @@ use std::time::Duration;
 use assign::assign;
 use async_stream::try_stream;
 use futures_core::stream::Stream;
-use ruma_client_api::r0::sync::sync_events::{
-    Filter as SyncFilter, Request as SyncRequest, Response as SyncResponse,
+use ruma_client_api::r0::{
+    account::register::{self, RegistrationKind},
+    session::login::{self, LoginInfo, UserIdentifier},
+    sync::sync_events,
 };
 use ruma_common::presence::PresenceState;
 use ruma_identifiers::DeviceId;
 
-use super::{Client, Error, Identification, Session};
+use super::{Client, Error};
 
+/// Client-API specific functionality of `Client`.
 impl Client {
     /// Log in with a username and password.
     ///
-    /// In contrast to `api::r0::session::login::call()`, this method stores the
-    /// session data returned by the endpoint in this client, instead of
-    /// returning it.
+    /// In contrast to [`request`], this method stores the access token returned by the endpoint in
+    /// this client, in addition to returning it.
     pub async fn log_in(
         &self,
         user: &str,
         password: &str,
         device_id: Option<&DeviceId>,
         initial_device_display_name: Option<&str>,
-    ) -> Result<Session, Error<ruma_client_api::Error>> {
-        use ruma_client_api::r0::session::login::{
-            LoginInfo, Request as LoginRequest, UserIdentifier,
-        };
-
+    ) -> Result<login::Response, Error<ruma_client_api::Error>> {
         let response = self
             .request(assign!(
-                LoginRequest::new(
+                login::Request::new(
                     LoginInfo::Password { identifier: UserIdentifier::MatrixId(user), password }
                 ), {
                     device_id,
@@ -39,92 +37,61 @@ impl Client {
             ))
             .await?;
 
-        let session = Session {
-            access_token: response.access_token,
-            identification: Some(Identification {
-                device_id: response.device_id,
-                user_id: response.user_id,
-            }),
-        };
-        *self.0.session.lock().unwrap() = Some(session.clone());
+        *self.0.access_token.lock().unwrap() = Some(response.access_token.clone());
 
-        Ok(session)
+        Ok(response)
     }
 
-    /// Register as a guest. In contrast to `api::r0::account::register::call()`,
-    /// this method stores the session data returned by the endpoint in this
-    /// client, instead of returning it.
+    /// Register as a guest.
+    ///
+    /// In contrast to [`request`], this method stores the access token returned by the endpoint in
+    /// this client, in addition to returning it.
     pub async fn register_guest(
         &self,
-    ) -> Result<Session, Error<ruma_client_api::r0::uiaa::UiaaResponse>> {
-        use ruma_client_api::r0::account::register::{self, RegistrationKind};
-
+    ) -> Result<register::Response, Error<ruma_client_api::r0::uiaa::UiaaResponse>> {
         let response = self
             .request(assign!(register::Request::new(), { kind: RegistrationKind::Guest }))
             .await?;
 
-        let session = Session {
-            // since we supply inhibit_login: false above, the access token needs to be there
-            // TODO: maybe unwrap is not the best solution though
-            access_token: response.access_token.unwrap(),
-            identification: Some(Identification {
-                // same as access_token
-                device_id: response.device_id.unwrap(),
-                user_id: response.user_id,
-            }),
-        };
-        *self.0.session.lock().unwrap() = Some(session.clone());
+        *self.0.access_token.lock().unwrap() = response.access_token.clone();
 
-        Ok(session)
+        Ok(response)
     }
 
     /// Register as a new user on this server.
     ///
-    /// In contrast to `api::r0::account::register::call()`, this method stores
-    /// the session data returned by the endpoint in this client, instead of
-    /// returning it.
+    /// In contrast to [`request`], this method stores the access token returned by the endpoint in
+    /// this client, in addition to returning it.
     ///
-    /// The username is the local part of the returned user_id. If it is
-    /// omitted from this request, the server will generate one.
+    /// The username is the local part of the returned user_id. If it is omitted from this request,
+    /// the server will generate one.
     pub async fn register_user(
         &self,
         username: Option<&str>,
         password: &str,
-    ) -> Result<Session, Error<ruma_client_api::r0::uiaa::UiaaResponse>> {
-        use ruma_client_api::r0::account::register;
-
+    ) -> Result<register::Response, Error<ruma_client_api::r0::uiaa::UiaaResponse>> {
         let response = self
             .request(assign!(register::Request::new(), { username, password: Some(password) }))
             .await?;
 
-        let session = Session {
-            // since we supply inhibit_login: false above, the access token needs to be there
-            // TODO: maybe unwrap is not the best solution though
-            access_token: response.access_token.unwrap(),
-            identification: Some(Identification {
-                // same as access_token
-                device_id: response.device_id.unwrap(),
-                user_id: response.user_id,
-            }),
-        };
-        *self.0.session.lock().unwrap() = Some(session.clone());
+        *self.0.access_token.lock().unwrap() = response.access_token.clone();
 
-        Ok(session)
+        Ok(response)
     }
 
     /// Convenience method that represents repeated calls to the sync_events endpoint as a stream.
     pub fn sync<'a>(
         &self,
-        filter: Option<&'a SyncFilter<'a>>,
+        filter: Option<&'a sync_events::Filter<'a>>,
         mut since: String,
         set_presence: &'a PresenceState,
         timeout: Option<Duration>,
-    ) -> impl Stream<Item = Result<SyncResponse, Error<ruma_client_api::Error>>> + 'a {
+    ) -> impl Stream<Item = Result<sync_events::Response, Error<ruma_client_api::Error>>> + 'a {
         let client = self.clone();
         try_stream! {
             loop {
                 let response = client
-                    .request(assign!(SyncRequest::new(), {
+                    .request(assign!(sync_events::Request::new(), {
                         filter,
                         since: Some(&since),
                         set_presence,
