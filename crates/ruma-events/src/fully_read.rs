10f65e5826f181fb5579f090f4994db6738ae291
Kévin Commaille <zecakeh@tedomum.fr>
2021-05-08 20:29:27 +0200

    events: Move FullyReadEvent from EphemeralRoomEvent to BasicEvent

--- crates/ruma-events/src/fully_read.rs
+++ crates/ruma-events/src/fully_read.rs
@@ -1,19 +1,19 @@
 //! Types for the *m.fully_read* event.
 
-use ruma_events_macros::EphemeralRoomEventContent;
+use ruma_events_macros::BasicEventContent;
 use ruma_identifiers::EventId;
 use serde::{Deserialize, Serialize};
 
-use crate::EphemeralRoomEvent;
+use crate::BasicEvent;
 
 /// The current location of the user's read marker in a room.
 ///
 /// This event appears in the user's room account data for the room the marker is applicable
 /// for.
-pub type FullyReadEvent = EphemeralRoomEvent<FullyReadEventContent>;
+pub type FullyReadEvent = BasicEvent<FullyReadEventContent>;
 
 /// The payload for `FullyReadEvent`.
-#[derive(Clone, Debug, Deserialize, Serialize, EphemeralRoomEventContent)]
+#[derive(Clone, Debug, Deserialize, Serialize, BasicEventContent)]
 #[ruma_event(type = "m.fully_read")]
 pub struct FullyReadEventContent {
     /// The event the user's read marker is located at in the room.
