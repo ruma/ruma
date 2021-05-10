ruma/ruma

commit e8d81da49281717ad46f36b76c40fc3853d2aafd

events: Remove AnyEvent deserialization benchmark

crates/ruma-events/benches/event_deserialize.rs
@@ -9,7 +9,7 @@
 #[cfg(feature = "criterion")]
 use criterion::{criterion_group, criterion_main, Criterion};
 use ruma_events::{
-    room::power_levels::PowerLevelsEventContent, AnyEvent, AnyRoomEvent, AnyStateEvent, StateEvent,
+    room::power_levels::PowerLevelsEventContent, AnyRoomEvent, AnyStateEvent, StateEvent,
 };
 use ruma_serde::Raw;
 use serde_json::json;
@@ -45,20 +45,6 @@ fn power_levels() -> serde_json::Value {
             "age": 45
         }
     })
-}
-
-#[cfg(feature = "criterion")]
-fn deserialize_any_event(c: &mut Criterion) {
-    let json_data = power_levels();
-
-    c.bench_function("deserialize to `AnyEvent`", |b| {
-        b.iter(|| {
-            let _ = serde_json::from_value::<Raw<AnyEvent>>(json_data.clone())
-                .unwrap()
-                .deserialize()
-                .unwrap();
-        })
-    });
 }
 
 #[cfg(feature = "criterion")]

commit 3c58faac6e60dbb46628dd3e317de0810a19bb7e

events: Fix event deserialization benchmark
Regression introduced in e8d81da49281717ad46f36b76c40fc3853d2aafd

crates/ruma-events/benches/event_deserialize.rs
@@ -94,7 +94,6 @@ fn deserialize_specific_event(c: &mut Criterion) {
 #[cfg(feature = "criterion")]
 criterion_group!(
     benches,
-    deserialize_any_event,
     deserialize_any_room_event,
     deserialize_any_state_event,
     deserialize_specific_event