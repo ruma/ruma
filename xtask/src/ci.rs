dcff455c19cf14ce61ed24b43ffdc75e4655b317
Devin Ragotzy <devin.ragotzy@gmail.com>
2021-05-11 15:59:44 -0400

    ci: Enforce sorted dependencies with cargo-sort

--- xtask/src/ci.rs
+++ xtask/src/ci.rs
@@ -55,6 +55,15 @@ impl CiTask {
     fn build_nightly(&self) -> xshell::Result<()> {
         let fmt_res = cmd!("rustup run nightly cargo fmt -- --check").run();
         let clippy_res = cmd!("rustup run nightly cargo ruma-clippy -D warnings").run();
-        fmt_res.and(clippy_res)
+
+        let already_installed = cmd!("rustup run nightly cargo sort -V")
+            .read()
+            .map_or(false, |stdout| !stdout.contains("error"));
+        if !already_installed {
+            cmd!("rustup run nightly cargo install cargo-sort").run()?;
+        }
+        let sort_res = cmd!("cargo sort --workspace --grouped --check").run();
+
+        fmt_res.and(clippy_res).and(sort_res)
     }
 }
