dd940f810e5a1442cf83377cb006c0b0c01c6c9c
Kévin Commaille <zecakeh@tedomum.fr>
2021-05-11 09:11:11 +0200

    common: Use ruma-serde derives for ReceiptType

--- crates/ruma-common/src/receipt.rs
+++ crates/ruma-common/src/receipt.rs
@@ -1,9 +1,9 @@
 //! Common types for receipts.
 
-use ruma_serde::StringEnum;
+use ruma_serde::{OrdAsRefStr, PartialEqAsRefStr, PartialOrdAsRefStr, StringEnum};
 
 /// The type of receipt.
-#[derive(Clone, Debug, PartialOrd, Ord, PartialEq, Eq, StringEnum)]
+#[derive(Clone, Debug, PartialOrdAsRefStr, OrdAsRefStr, PartialEqAsRefStr, Eq, StringEnum)]
 pub enum ReceiptType {
     /// m.read
     #[ruma_enum(rename = "m.read")]
