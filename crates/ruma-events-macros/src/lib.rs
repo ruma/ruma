1aaf7786164bf59c966137935820e304effe19e9
Jonathan de Jong <jonathandejong02@gmail.com>
2021-05-14 11:02:35 +0000

    cargo fmt

--- crates/ruma-events-macros/src/lib.rs
+++ crates/ruma-events-macros/src/lib.rs
@@ -48,8 +48,8 @@ mod event_parse;
 /// }
 /// ```
 /// (The argument to `kind` has to be a valid identifier for `<EventKind as Parse>::parse`)
-//// TODO: Change above (`<EventKind as Parse>::parse`) to [] after fully qualified syntax is supported:
-////  https://github.com/rust-lang/rust/issues/74563
+//// TODO: Change above (`<EventKind as Parse>::parse`) to [] after fully qualified syntax is
+//// supported:  https://github.com/rust-lang/rust/issues/74563
 #[proc_macro]
 pub fn event_enum(input: TokenStream) -> TokenStream {
     let event_enum_input = syn::parse_macro_input!(input as EventEnumInput);
