a59c43321f28d47a5e50a060945a93afd189ee81
Jonas Platte <jplatte+git@posteo.de>
2021-07-07 16:14:41 +0200

    api-macros: Refactor unique_lifetimes_to_tokens

--- crates/ruma-api-macros/src/util.rs
+++ crates/ruma-api-macros/src/util.rs
@@ -12,12 +12,11 @@ pub(crate) fn unique_lifetimes_to_tokens<'a, I: IntoIterator<Item = &'a Lifetime
     lifetimes: I,
 ) -> TokenStream {
     let lifetimes = lifetimes.into_iter().collect::<BTreeSet<_>>();
-    (!lifetimes.is_empty())
-        .then(|| {
-            let lifetimes = quote! { #( #lifetimes ),* };
-            quote! { < #lifetimes > }
-        })
-        .unwrap_or_default()
+    if lifetimes.is_empty() {
+        TokenStream::new()
+    } else {
+        quote! { < #( #lifetimes ),* > }
+    }
 }
 
 pub(crate) fn is_valid_endpoint_path(string: &str) -> bool {
