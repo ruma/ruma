c33920d8ae093c7210a0a19a503cecc03de10826
Jonathan de Jong <jonathan@automatia.nl>
2021-08-17 14:48:10 +0200

    Use thiserror in id-validation, add more detail to mxc-uri errors

--- crates/ruma-identifiers-validation/src/error.rs
+++ crates/ruma-identifiers-validation/src/error.rs
@@ -1,66 +1,77 @@
 //! Error conditions.
 
-use std::fmt;
-
 /// An error encountered when trying to parse an invalid ID string.
-#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, thiserror::Error)]
 #[non_exhaustive]
 pub enum Error {
     /// The client secret is empty.
+    #[error("client secret is empty")]
     EmptyClientSecret,
 
     /// The room name is empty.
+    #[error("room name is empty")]
     EmptyRoomName,
 
     /// The room version ID is empty.
+    #[error("room version ID is empty")]
     EmptyRoomVersionId,
 
     /// The ID's localpart contains invalid characters.
     ///
     /// Only relevant for user IDs.
+    #[error("localpart contains invalid characters")]
     InvalidCharacters,
 
     /// The key algorithm is invalid (e.g. empty).
+    #[error("invalid key algorithm specified")]
     InvalidKeyAlgorithm,
 
     /// The key version contains outside of [a-zA-Z0-9_].
+    #[error("key ID version contains invalid characters")]
     InvalidKeyVersion,
 
     /// The mxc:// isn't a valid Matrix Content URI.
-    InvalidMxcUri,
+    #[error("invalid Matrix Content URI: {0}")]
+    InvalidMxcUri(#[from] MxcUriError),
 
     /// The server name part of the the ID string is not a valid server name.
+    #[error("server name is not a valid IP address or domain name")]
     InvalidServerName,
 
     /// The ID exceeds 255 bytes (or 32 codepoints for a room version ID).
+    #[error("ID exceeds 255 bytes")]
     MaximumLengthExceeded,
 
     /// The ID is missing the colon delimiter between localpart and server name, or between key
     /// algorithm and key name / version.
+    #[error("required colon is missing")]
     MissingDelimiter,
 
     /// The ID is missing the correct leading sigil.
+    #[error("leading sigil is incorrect or missing")]
     MissingLeadingSigil,
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let message = match self {
-            Error::EmptyClientSecret => "client secret is empty",
-            Error::EmptyRoomName => "room name is empty",
-            Error::EmptyRoomVersionId => "room version ID is empty",
-            Error::InvalidCharacters => "localpart contains invalid characters",
-            Error::InvalidKeyAlgorithm => "invalid key algorithm specified",
-            Error::InvalidKeyVersion => "key ID version contains invalid characters",
-            Error::InvalidMxcUri => "the mxc:// isn't a valid Matrix Content URI",
-            Error::InvalidServerName => "server name is not a valid IP address or domain name",
-            Error::MaximumLengthExceeded => "ID exceeds 255 bytes",
-            Error::MissingDelimiter => "required colon is missing",
-            Error::MissingLeadingSigil => "leading sigil is incorrect or missing",
-        };
+/// An error occurred while validating an MXC URI.
+#[derive(Copy, Clone, Debug, Eq, Hash, PartialEq, thiserror::Error)]
+#[non_exhaustive]
+pub enum MxcUriError {
+    /// MXC URI did not start with `mxc://`.
+    #[error("MXC URI schema was not mxc://")]
+    WrongSchema,
 
-        write!(f, "{}", message)
-    }
+    /// MXC URI did not have first slash, required for `server.name/media_id`.
+    #[error("MXC URI does not have first slash")]
+    MissingSlash,
+
+    /// Media identifier malformed due to invalid characters detected.
+    ///
+    /// Valid characters are (in regex notation) `[A-Za-z0-9_-]+`.
+    /// See [here](https://matrix.org/docs/spec/client_server/r0.6.1#id408) for more details.
+    #[error("Media Identifier malformed, invalid characters")]
+    MediaIdMalformed,
+
+    /// Server identifier malformed: invalid IP or domain name.
+    #[error("invalid Server Name")]
+    ServerNameMalformed,
 }
-
-impl std::error::Error for Error {}
