85fc0b39da5f7fe2f2ebb51f354c68d77b54aade
Jonas Platte <jplatte+git@posteo.de>
2021-08-18 18:04:05 +0200

    identifiers-validation: Don't validate key ID version with feature = "compat"

--- crates/ruma-identifiers-validation/src/key_id.rs
+++ crates/ruma-identifiers-validation/src/key_id.rs
@@ -6,11 +6,13 @@ pub fn validate(s: &str) -> Result<NonZeroU8, Error> {
     let colon_idx = NonZeroU8::new(s.find(':').ok_or(Error::MissingDelimiter)? as u8)
         .ok_or(Error::InvalidKeyAlgorithm)?;
 
+    #[cfg(not(feature = "compat"))]
     validate_version(&s[colon_idx.get() as usize + 1..])?;
 
     Ok(colon_idx)
 }
 
+#[cfg(not(feature = "compat"))]
 fn validate_version(version: &str) -> Result<(), Error> {
     if version.is_empty() {
         return Err(Error::EmptyRoomVersionId);
