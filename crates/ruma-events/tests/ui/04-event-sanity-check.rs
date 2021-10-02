3ee4ac08fb6b7c67144eb5924e515ca4598d7c0d
Jonas Platte <jplatte+git@posteo.de>
2021-10-02 13:08:19 +0200

    events: Fix a test case

--- crates/ruma-events/tests/ui/04-event-sanity-check.rs
+++ crates/ruma-events/tests/ui/04-event-sanity-check.rs
@@ -2,13 +2,19 @@
 // https://github.com/rust-lang/rust/issues/55779
 extern crate serde;
 
+use ruma_common::MilliSecondsSinceUnixEpoch;
 use ruma_events::{StateEventContent, Unsigned};
 use ruma_events_macros::Event;
+use ruma_identifiers::{EventId, RoomId, UserId};
 
 /// State event.
 #[derive(Clone, Debug, Event)]
 pub struct StateEvent<C: StateEventContent> {
     pub content: C,
+    pub event_id: EventId,
+    pub sender: UserId,
+    pub origin_server_ts: MilliSecondsSinceUnixEpoch,
+    pub room_id: RoomId,
     pub state_key: String,
     pub prev_content: Option<C>,
     pub unsigned: Unsigned,
