852833452660a6486b080ef439067f9c80772b55
imbolc <me@imbolc.name>
2021-11-02 19:43:44 +0300

    Broken `empty_de_int_some` test

--- crates/ruma-serde/tests/empty_strings.rs
+++ crates/ruma-serde/tests/empty_strings.rs
@@ -12,6 +12,16 @@ struct StringStruct {
 }
 
 #[derive(Serialize, Deserialize, PartialEq, Debug)]
+struct IntStruct {
+    #[serde(
+        default,
+        deserialize_with = "ruma_serde::empty_string_as_none",
+        serialize_with = "ruma_serde::none_as_empty_string"
+    )]
+    x: Option<i32>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
 struct NoneStruct {
     #[serde(skip_serializing_if = "Option::is_none")]
     x: Option<String>,
@@ -26,10 +36,29 @@ fn empty_se() {
 }
 
 #[test]
-fn empty_de() {
+fn empty_de_string_none() {
     let string = StringStruct { x: None };
     let none = NoneStruct { x: None };
     assert_eq!(from_json_value::<StringStruct>(json!({"x": ""})).unwrap(), string);
     assert_eq!(from_json_value::<StringStruct>(json!({})).unwrap(), string);
     assert_eq!(from_json_value::<NoneStruct>(json!({})).unwrap(), none);
+}
+
+#[test]
+fn empty_de_string_some() {
+    let string = StringStruct { x: Some("foo".into()) };
+    assert_eq!(from_json_value::<StringStruct>(json!({"x": "foo"})).unwrap(), string);
+}
+
+#[test]
+fn empty_de_int_none() {
+    let expected = IntStruct { x: None };
+    assert_eq!(from_json_value::<IntStruct>(json!({"x": ""})).unwrap(), expected);
+    assert_eq!(from_json_value::<IntStruct>(json!({})).unwrap(), expected);
+}
+
+#[test]
+fn empty_de_int_some() {
+    let expected = IntStruct { x: Some(1) };
+    assert_eq!(from_json_value::<IntStruct>(json!({"x": 1})).unwrap(), expected);
 }
