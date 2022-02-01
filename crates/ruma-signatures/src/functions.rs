02eb567b69e93d984e0420712e3b6d20050f0254
Jonathan de Jong <jonathan@automatia.nl>
2022-02-01 13:34:04 +0100

    revert rename

--- crates/ruma-signatures/src/functions.rs
+++ crates/ruma-signatures/src/functions.rs
@@ -372,9 +372,9 @@ pub fn reference_hash(
     value: &CanonicalJsonObject,
     version: &RoomVersionId,
 ) -> Result<String, Error> {
-    let redacted = redact(value, version)?;
+    let redacted_value = redact(value, version)?;
 
-    let json = canonical_json_with_fields_to_remove(&redacted, REFERENCE_HASH_FIELDS_TO_REMOVE)?;
+    let json = canonical_json_with_fields_to_remove(&redacted_value, REFERENCE_HASH_FIELDS_TO_REMOVE)?;
     if json.len() > MAX_PDU_BYTES {
         return Err(Error::PduSize);
     }
