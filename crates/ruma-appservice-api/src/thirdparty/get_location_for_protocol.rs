d8d116a006d83746bfd78f0d96d13ab208ac7842
Jonathan de Jong <jonathan@automatia.nl>
2022-02-13 17:14:14 +0100

    ruma-appservice-api: Refactor file structure and docs

--- crates/ruma-appservice-api/src/thirdparty/get_location_for_protocol.rs
+++ crates/ruma-appservice-api/src/thirdparty/get_location_for_protocol.rs
@@ -1,4 +1,58 @@
+//! `GET /_matrix/app/*/thirdparty/location/{protocol}`
+//!
 //! Endpoint to retrieve a list of Matrix portal rooms that lead to the matched third party
 //! location.
 
-pub mod v1;
+pub mod v1 {
+    //! `/v1/` ([spec])
+    //!
+    //! [spec]: https://spec.matrix.org/v1.2/application-service-api/#get_matrixappv1thirdpartylocationprotocol
+
+    use std::collections::BTreeMap;
+
+    use ruma_api::ruma_api;
+    use ruma_common::thirdparty::Location;
+
+    ruma_api! {
+        metadata: {
+            description: "Fetches third party locations for a protocol.",
+            method: GET,
+            name: "get_location_for_protocol",
+            stable_path: "/_matrix/app/v1/thirdparty/location/:protocol",
+            rate_limited: false,
+            authentication: QueryOnlyAccessToken,
+            added: 1.0,
+        }
+
+        request: {
+            /// The protocol used to communicate to the third party network.
+            #[ruma_api(path)]
+            pub protocol: &'a str,
+
+            /// One or more custom fields to help identify the third party location.
+            // The specification is incorrect for this parameter. See matrix-org/matrix-doc#2352.
+            #[ruma_api(query_map)]
+            pub fields: BTreeMap<String, String>,
+        }
+
+        response: {
+            /// List of matched third party locations.
+            #[ruma_api(body)]
+            pub locations: Vec<Location>,
+        }
+    }
+
+    impl<'a> Request<'a> {
+        /// Creates a new `Request` with the given protocol.
+        pub fn new(protocol: &'a str) -> Self {
+            Self { protocol, fields: BTreeMap::new() }
+        }
+    }
+
+    impl Response {
+        /// Creates a new `Response` with the given locations.
+        pub fn new(locations: Vec<Location>) -> Self {
+            Self { locations }
+        }
+    }
+}
--- crates/ruma-appservice-api/src/thirdparty/get_protocol.rs
+++ crates/ruma-appservice-api/src/thirdparty/get_protocol.rs
@@ -1,4 +1,51 @@
+//! `GET /_matrix/app/*/thirdparty/protocol/{protocol}`
+//!
 //! Endpoint to present clients with specific information about the various third party networks
 //! that an application service supports.
 
-pub mod v1;
+pub mod v1 {
+    //! `/v1/` ([spec])
+    //!
+    //! [spec]: https://spec.matrix.org/v1.2/application-service-api/#get_matrixappv1thirdpartyprotocolprotocol
+
+    use ruma_api::ruma_api;
+    use ruma_common::thirdparty::Protocol;
+
+    ruma_api! {
+        metadata: {
+            description: "Fetches the metadata from the homeserver about a particular third party protocol.",
+            method: GET,
+            name: "get_protocol",
+            stable_path: "/_matrix/app/v1/thirdparty/protocol/:protocol",
+            rate_limited: false,
+            authentication: QueryOnlyAccessToken,
+            added: 1.0,
+        }
+
+        request: {
+            /// The name of the protocol.
+            #[ruma_api(path)]
+            pub protocol: &'a str,
+        }
+
+        response: {
+            /// Metadata about the protocol.
+            #[ruma_api(body)]
+            pub protocol: Protocol,
+        }
+    }
+
+    impl<'a> Request<'a> {
+        /// Creates a new `Request` with the given protocol name.
+        pub fn new(protocol: &'a str) -> Self {
+            Self { protocol }
+        }
+    }
+
+    impl Response {
+        /// Creates a new `Response` with the given protocol.
+        pub fn new(protocol: Protocol) -> Self {
+            Self { protocol }
+        }
+    }
+}
