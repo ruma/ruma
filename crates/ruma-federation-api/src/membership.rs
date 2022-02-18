b3d4b27b911bcff91b80aa639a75754b4d230f1e
Jonathan de Jong <jonathan@automatia.nl>
2022-02-18 13:54:50 +0100

    federation-api: Set a consistent naming scheme for membership endpoints

--- crates/ruma-federation-api/src/membership.rs
+++ crates/ruma-federation-api/src/membership.rs
@@ -2,6 +2,6 @@
 
 pub mod create_invite;
 pub mod create_join_event;
-pub mod create_join_event_template;
 pub mod create_leave_event;
-pub mod get_leave_event;
+pub mod prepare_join_event;
+pub mod prepare_leave_event;
