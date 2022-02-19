62ede722b8e0f742f5be3a6881792f741d3c81ad
Jonas Platte <jplatte+git@posteo.de>
2022-02-19 01:09:08 +0100

    xtask: Fix VersionExt::is_next

--- xtask/src/release.rs
+++ xtask/src/release.rs
@@ -416,6 +416,7 @@ impl VersionExt for Version {
                 return true;
             }
 
+            next.patch = 0;
             next.minor += 1;
             if next == *version {
                 return true;
@@ -426,6 +427,7 @@ impl VersionExt for Version {
                 return true;
             }
 
+            next.minor = 0;
             next.major += 1;
             if next == *version {
                 return true;
