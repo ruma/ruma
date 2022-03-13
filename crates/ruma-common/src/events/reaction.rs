2d88df7fb70418d3ed7ffae54bfb057554d64e4b
Kévin Commaille <zecakeh@tedomum.fr>
2022-03-13 12:59:05 +0100

    common: Clarify that reactions are not all emojis
    According to MSC2677

--- crates/ruma-common/src/events/reaction.rs
+++ crates/ruma-common/src/events/reaction.rs
@@ -32,23 +32,27 @@ impl From<Relation> for ReactionEventContent {
     }
 }
 
-/// The relation that contains info which event the reaction is applying to.
+/// Information about an annotation relation.
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
 #[serde(tag = "rel_type", rename = "m.annotation")]
 pub struct Relation {
-    /// The event that is being reacted to.
+    /// The event that is being annoted.
     pub event_id: Box<EventId>,
 
-    /// A string that holds the emoji reaction.
-    #[serde(rename = "key")]
-    pub emoji: String,
+    /// A string that indicates the annotation being applied.
+    ///
+    /// When sending emoji reactions, this field should include the colourful variation-16 when
+    /// applicable.
+    ///
+    /// Clients should render reactions that have a long `key` field in a sensible manner.
+    pub key: String,
 }
 
 impl Relation {
-    /// Creates a new `Relation` with the given event ID and emoji.
-    pub fn new(event_id: Box<EventId>, emoji: String) -> Self {
-        Self { event_id, emoji }
+    /// Creates a new `Relation` with the given event ID and key.
+    pub fn new(event_id: Box<EventId>, key: String) -> Self {
+        Self { event_id, key }
     }
 }
 
@@ -74,8 +78,8 @@ mod tests {
 
         assert_matches!(
             from_json_value::<ReactionEventContent>(json).unwrap(),
-            ReactionEventContent { relates_to: Relation { event_id, emoji } }
-            if event_id == ev_id && emoji == "🦛"
+            ReactionEventContent { relates_to: Relation { event_id, key } }
+            if event_id == ev_id && key == "🦛"
         );
     }
 }
