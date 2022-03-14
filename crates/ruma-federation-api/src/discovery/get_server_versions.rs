4068ea14dc1aa196a173a09beec176826aaa9169
Jonathan de Jong <jonathan@automatia.nl>
2022-03-14 11:48:43 +0100

    fix CI

--- crates/ruma-federation-api/src/discovery/get_server_versions.rs
+++ crates/ruma-federation-api/src/discovery/get_server_versions.rs
@@ -3,15 +3,14 @@
 pub mod msc3723 {
     //! [GET /_matrix/federation/versions](https://github.com/matrix-org/matrix-doc/pull/3723)
 
-    use ruma_api::ruma_api;
+    use ruma_common::api::ruma_api;
 
     ruma_api! {
         metadata: {
             description: "Get the supported matrix versions of this homeserver",
             method: GET,
             name: "get_server_versions",
-            path: "/_matrix/federation/unstable/org.matrix.msc3723/versions",
-            unstable: "/_matrix/federation/unstable/org.matrix.msc3723/versions",
+            unstable_path: "/_matrix/federation/unstable/org.matrix.msc3723/versions",
             rate_limited: false,
             authentication: None,
         }
