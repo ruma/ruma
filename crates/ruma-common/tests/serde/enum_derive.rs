56cf3ce94cdc61d29df14cba06363ad2733a9668
Jonas Platte <jplatte@matrix.org>
2022-06-07 18:15:05 +0200

    Allow buggy clippy lint on a per-crate basis
    The previous method of allowing it through .cargo/config.toml doesn't
    seem to work anymore.

--- crates/ruma-common/tests/serde/enum_derive.rs
+++ crates/ruma-common/tests/serde/enum_derive.rs
@@ -1,3 +1,6 @@
+// https://github.com/rust-lang/rust-clippy/issues/8875
+#![allow(clippy::derive_partial_eq_without_eq)]
+
 use ruma_common::serde::StringEnum;
 use serde_json::{from_value as from_json_value, json, to_value as to_json_value};
 
