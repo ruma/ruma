7d2380c5a1cb9b6cba54fb2443b4ead87447f960
Jonas Platte <jplatte@matrix.org>
2022-06-08 11:14:33 +0200

    identifiers-validation: Make localpart_is_fully_conforming const

--- crates/ruma-identifiers-validation/src/user_id.rs
+++ crates/ruma-identifiers-validation/src/user_id.rs
@@ -17,18 +17,54 @@ pub fn validate(s: &str) -> Result<(), Error> {
 /// localparts. User IDs that don't even meet the historical user ID restrictions exist in the wild
 /// due to Synapse allowing them over federation. This will likely be fixed in an upcoming room
 /// version; see [MSC2828](https://github.com/matrix-org/matrix-spec-proposals/pull/2828).
-pub fn localpart_is_fully_conforming(localpart: &str) -> Result<bool, Error> {
+pub const fn localpart_is_fully_conforming(localpart: &str) -> Result<bool, Error> {
     // See https://spec.matrix.org/v1.2/appendices/#user-identifiers
-    let is_fully_conforming = localpart
-        .bytes()
-        .all(|b| matches!(b, b'0'..=b'9' | b'a'..=b'z' | b'-' | b'.' | b'=' | b'_' | b'/'));
+    let is_fully_conforming = {
+        // Non-`const` form:
+        //
+        // localpart
+        //     .bytes()
+        //     .all(|b| matches!(b, b'0'..=b'9' | b'a'..=b'z' | b'-' | b'.' | b'=' | b'_' | b'/'))
+        let mut bytes = localpart.as_bytes();
+        loop {
+            if let Some((b, rest)) = bytes.split_first() {
+                if matches!(b, b'0'..=b'9' | b'a'..=b'z' | b'-' | b'.' | b'=' | b'_' | b'/') {
+                    bytes = rest;
+                } else {
+                    break false;
+                }
+            } else {
+                break true;
+            }
+        }
+    };
 
     // If it's not fully conforming, check if it contains characters that are also disallowed
     // for historical user IDs. If there are, return an error.
     // See https://spec.matrix.org/v1.2/appendices/#historical-user-ids
     #[cfg(not(feature = "compat"))]
-    if !is_fully_conforming && localpart.bytes().any(|b| b < 0x21 || b == b':' || b > 0x7E) {
-        return Err(Error::InvalidCharacters);
+    if !is_fully_conforming {
+        let has_invalid_chars = {
+            // Non-`const` form:
+            //
+            // localpart.bytes().any(|b| b < 0x21 || b == b':' || b > 0x7E);
+            let mut bytes = localpart.as_bytes();
+            loop {
+                if let Some((&b, rest)) = bytes.split_first() {
+                    if b < 0x21 || b == b':' || b > 0x7E {
+                        break true;
+                    } else {
+                        bytes = rest;
+                    }
+                } else {
+                    break false;
+                }
+            }
+        };
+
+        if has_invalid_chars {
+            return Err(Error::InvalidCharacters);
+        }
     }
 
     Ok(is_fully_conforming)
