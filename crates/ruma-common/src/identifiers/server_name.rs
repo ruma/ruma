4799da6e4d55b28f5f90473667a07d51967a0aa3
Kévin Commaille <zecakeh@tedomum.fr>
2022-06-11 22:36:41 +0200

    identifiers: Make more test assertions more helpful

--- crates/ruma-common/src/identifiers/server_name.rs
+++ crates/ruma-common/src/identifiers/server_name.rs
@@ -57,58 +57,58 @@ mod tests {
 
     #[test]
     fn ipv4_host() {
-        assert!(<&ServerName>::try_from("127.0.0.1").is_ok());
+        <&ServerName>::try_from("127.0.0.1").unwrap();
     }
 
     #[test]
     fn ipv4_host_and_port() {
-        assert!(<&ServerName>::try_from("1.1.1.1:12000").is_ok());
+        <&ServerName>::try_from("1.1.1.1:12000").unwrap();
     }
 
     #[test]
     fn ipv6() {
-        assert!(<&ServerName>::try_from("[::1]").is_ok());
+        <&ServerName>::try_from("[::1]").unwrap();
     }
 
     #[test]
     fn ipv6_with_port() {
-        assert!(<&ServerName>::try_from("[1234:5678::abcd]:5678").is_ok());
+        <&ServerName>::try_from("[1234:5678::abcd]:5678").unwrap();
     }
 
     #[test]
     fn dns_name() {
-        assert!(<&ServerName>::try_from("example.com").is_ok());
+        <&ServerName>::try_from("example.com").unwrap();
     }
 
     #[test]
     fn dns_name_with_port() {
-        assert!(<&ServerName>::try_from("ruma.io:8080").is_ok());
+        <&ServerName>::try_from("ruma.io:8080").unwrap();
     }
 
     #[test]
     fn empty_string() {
-        assert!(<&ServerName>::try_from("").is_err());
+        <&ServerName>::try_from("").unwrap_err();
     }
 
     #[test]
     fn invalid_ipv6() {
-        assert!(<&ServerName>::try_from("[test::1]").is_err());
+        <&ServerName>::try_from("[test::1]").unwrap_err();
     }
 
     #[test]
     fn ipv4_with_invalid_port() {
-        assert!(<&ServerName>::try_from("127.0.0.1:").is_err());
+        <&ServerName>::try_from("127.0.0.1:").unwrap_err();
     }
 
     #[test]
     fn ipv6_with_invalid_port() {
-        assert!(<&ServerName>::try_from("[fe80::1]:100000").is_err());
-        assert!(<&ServerName>::try_from("[fe80::1]!").is_err());
+        <&ServerName>::try_from("[fe80::1]:100000").unwrap_err();
+        <&ServerName>::try_from("[fe80::1]!").unwrap_err();
     }
 
     #[test]
     fn dns_name_with_invalid_port() {
-        assert!(<&ServerName>::try_from("matrix.org:hello").is_err());
+        <&ServerName>::try_from("matrix.org:hello").unwrap_err();
     }
 
     #[test]
