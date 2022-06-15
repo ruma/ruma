d5d9dd6696cfe557216fbc14c7b55da2c1aea02b
Jonathan de Jong <jonathan@automatia.nl>
2022-06-15 11:20:03 +0200

    add MSC2666

---
+++ crates/ruma-client-api/src/membership/mutual_rooms.rs
@@ -0,0 +1,48 @@
+//! `GET /_matrix/client/*/user/mutual_rooms/{user_id}`
+
+pub mod unstable {
+    //! `/unstable/` ([spec])
+    //!
+    //! [spec]: https://github.com/matrix-org/matrix-spec-proposals/blob/hs/shared-rooms/proposals/2666-get-rooms-in-common.md
+
+    use ruma_common::{api::ruma_api, OwnedRoomId, UserId};
+
+    ruma_api! {
+        metadata: {
+            description: "Get mutual rooms with another user.",
+            method: GET,
+            name: "mutual_rooms",
+            unstable_path: "/_matrix/client/unstable/uk.half-shot.msc2666/user/mutual_rooms/:user_id",
+            rate_limited: true,
+            authentication: AccessToken,
+        }
+
+        request: {
+            /// The user to search mutual rooms for.
+            #[ruma_api(path)]
+            pub user_id: &'a UserId,
+        }
+
+        #[derive(Default)]
+        response: {
+            /// A list of rooms the user is in together with the authenticated user.
+            pub joined: Vec<OwnedRoomId>,
+        }
+
+        error: crate::Error
+    }
+
+    impl<'a> Request<'a> {
+        /// Creates a new `Request` with the given user id.
+        pub fn new(user_id: &'a UserId) -> Self {
+            Self { user_id }
+        }
+    }
+
+    impl Response {
+        /// Creates a `Response` with the given room ids.
+        pub fn new(joined: Vec<OwnedRoomId>) -> Self {
+            Self { joined }
+        }
+    }
+}
