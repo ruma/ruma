5b25f2f2234c6781a03779851352df175ad6a2b3
Kévin Commaille <zecakeh@tedomum.fr>
2022-09-30 19:17:39 +0200

    push-rule: Stabilize default room server ACL push rule

--- crates/ruma-common/src/push/predefined.rs
+++ crates/ruma-common/src/push/predefined.rs
@@ -26,7 +26,6 @@ impl Ruleset {
                 ConditionalPushRule::member_event(),
                 ConditionalPushRule::contains_display_name(),
                 ConditionalPushRule::tombstone(),
-                #[cfg(feature = "unstable-msc3786")]
                 ConditionalPushRule::server_acl(),
                 ConditionalPushRule::roomnotif(),
                 #[cfg(feature = "unstable-msc2677")]
@@ -174,14 +173,13 @@ impl ConditionalPushRule {
 
     /// Matches [room server ACLs].
     ///
-    /// [room server ACLs]: https://spec.matrix.org/v1.3/client-server-api/#server-access-control-lists-acls-for-rooms
-    #[cfg(feature = "unstable-msc3786")]
+    /// [room server ACLs]: https://spec.matrix.org/v1.4/client-server-api/#server-access-control-lists-acls-for-rooms
     pub fn server_acl() -> Self {
         Self {
             actions: vec![],
             default: true,
             enabled: true,
-            rule_id: ".org.matrix.msc3786.rule.room.server_acl".into(),
+            rule_id: ".m.rule.room.server_acl".into(),
             conditions: vec![
                 EventMatch { key: "type".into(), pattern: "m.room.server_acl".into() },
                 EventMatch { key: "state_key".into(), pattern: "".into() },
