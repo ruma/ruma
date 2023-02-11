4fec903047d67b6c262e0c214ed96afdcb623bb6
Kévin Commaille <zecakeh@tedomum.fr>
2023-02-11 11:24:25 +0100

    docs: Fix invalid link to the spec

--- crates/ruma-common/src/identifiers/event_id.rs
+++ crates/ruma-common/src/identifiers/event_id.rs
@@ -11,12 +11,11 @@ use super::ServerName;
 ///
 /// # Room versions
 ///
-/// Matrix specifies multiple [room versions](https://spec.matrix.org/v1.4/#room-versions) and the
-/// format of event identifiers differ between them. The original format used by room versions 1 and
-/// 2 uses a short pseudorandom "localpart" followed by the hostname and port of the originating
-/// homeserver. Later room versions change event identifiers to be a hash of the event encoded with
-/// Base64. Some of the methods provided by `EventId` are only relevant to the original event
-/// format.
+/// Matrix specifies multiple [room versions] and the format of event identifiers differ between
+/// them. The original format used by room versions 1 and 2 uses a short pseudorandom "localpart"
+/// followed by the hostname and port of the originating homeserver. Later room versions change
+/// event identifiers to be a hash of the event encoded with Base64. Some of the methods provided by
+/// `EventId` are only relevant to the original event format.
 ///
 /// ```
 /// # use ruma_common::EventId;
@@ -35,6 +34,7 @@ use super::ServerName;
 /// ```
 ///
 /// [event ID]: https://spec.matrix.org/v1.4/appendices/#room-ids-and-event-ids
+/// [room versions]: https://spec.matrix.org/v1.4/rooms/#complete-list-of-room-versions
 #[repr(transparent)]
 #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, IdZst)]
 #[ruma_id(validate = ruma_identifiers_validation::event_id::validate)]
