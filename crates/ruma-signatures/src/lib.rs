b3b8f097dc49de5c306df13c99bab56164dff123
Jonas Platte <jplatte+git@posteo.de>
2023-03-01 09:30:28 +0100

    Remove trailing whitespace

--- crates/ruma-signatures/src/lib.rs
+++ crates/ruma-signatures/src/lib.rs
@@ -89,7 +89,6 @@ fn split_id(id: &str) -> Result<(Algorithm, String), Error> {
     const EXTRA_ALLOWED: [u8; 3] = [b'_', b'+', b'/'];
     #[cfg(not(feature = "compat"))]
     const EXTRA_ALLOWED: [u8; 1] = [b'_'];
-      
     if !version.bytes().all(|ch| ch.is_ascii_alphanumeric() || EXTRA_ALLOWED.contains(&ch)) {
         return Err(Error::InvalidVersion(version.into()));
     }
