9050d036f96fda81d879a1b3107bb69a5b19b9ff
Michael Hollister <michael@futo.org>
2023-10-05 13:05:30 -0500

    Reverted changes in to_device serialiation test
    Signed-off-by: Michael Hollister <michael@futo.org>

--- crates/ruma-events/tests/it/to_device.rs
+++ crates/ruma-events/tests/it/to_device.rs
@@ -9,11 +9,8 @@ fn serialization() {
         owned_room_id!("!testroomid:example.org"),
         "SessId".into(),
         "SessKey".into(),
-        #[cfg(feature = "unstable-msc3061")]
-        true,
     );
 
-    #[cfg(not(feature = "unstable-msc3061"))]
     assert_eq!(
         to_json_value(&content).unwrap(),
         json!({
@@ -21,18 +18,6 @@ fn serialization() {
             "room_id": "!testroomid:example.org",
             "session_id": "SessId",
             "session_key": "SessKey",
-        })
-    );
-
-    #[cfg(feature = "unstable-msc3061")]
-    assert_eq!(
-        to_json_value(&content).unwrap(),
-        json!({
-            "algorithm": "m.megolm.v1.aes-sha2",
-            "room_id": "!testroomid:example.org",
-            "session_id": "SessId",
-            "session_key": "SessKey",
-            "org.matrix.msc3061.shared_history": true,
         })
     );
 }
