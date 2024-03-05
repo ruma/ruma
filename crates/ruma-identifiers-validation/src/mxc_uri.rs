013d694834d3ae7dc3a703fb0783eab3c4a05f0c
Tulir Asokan <tulir@maunium.net>
2024-03-05 23:39:26 +0200

    Fix MXC URI validation

--- crates/ruma-identifiers-validation/src/mxc_uri.rs
+++ crates/ruma-identifiers-validation/src/mxc_uri.rs
@@ -19,7 +19,7 @@ pub fn validate(uri: &str) -> Result<NonZeroU8, MxcUriError> {
     let media_id = &uri[index + 1..];
     // See: https://spec.matrix.org/v1.9/client-server-api/#security-considerations-5
     let media_id_is_valid =
-        media_id.bytes().all(|b| matches!(b, b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'-' ));
+        media_id.bytes().all(|b| matches!(b, b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'-' | b'_' ));
 
     if !media_id_is_valid {
         Err(MxcUriError::MediaIdMalformed)
