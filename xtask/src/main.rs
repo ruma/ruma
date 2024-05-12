6fb12c6e7f74b84e681f704c71718f8070f2a943
Kévin Commaille <zecakeh@tedomum.fr>
2024-05-12 15:45:06 +0200

    ci: Update version of Rust nightly

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -15,7 +15,7 @@ use serde::Deserialize;
 use serde_json::from_str as from_json_str;
 
 // Keep in sync with version in `rust-toolchain.toml` and `.github/workflows/ci.yml`
-const NIGHTLY: &str = "nightly-2024-02-14";
+const NIGHTLY: &str = "nightly-2024-05-09";
 
 mod cargo;
 mod ci;
