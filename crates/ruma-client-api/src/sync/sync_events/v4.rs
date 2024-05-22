4e14455990febab5a15c7b21b8a6b8f718bcc68f
Ivan Enderlin <ivan@mnt.io>
2024-05-22 15:36:24 +0200

    feat(client-api): `v4::SyncRequestList` has a new `include_heroes` field.
    [As specified by the MSC][MSC], this patch adds a new `include_heroes`
    field on `v4::SyncRequestList`.
    
    MSC: https://github.com/matrix-org/matrix-spec-proposals/blob/kegan/sync-v3/proposals/3575-sync.md#room-list-parameters

--- crates/ruma-client-api/src/sync/sync_events/v4.rs
+++ crates/ruma-client-api/src/sync/sync_events/v4.rs
@@ -294,9 +294,16 @@ pub struct SyncRequestList {
     #[serde(flatten)]
     pub room_details: RoomDetailsConfig,
 
-    /// If tombstoned rooms should be returned and if so, with what information attached
+    /// If tombstoned rooms should be returned and if so, with what information attached.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub include_old_rooms: Option<IncludeOldRooms>,
+
+    /// Return a stripped variant of membership events for the users used to calculate the room
+    /// name.
+    ///
+    /// Sticky.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub include_heroes: Option<bool>,
 
     /// Filters to apply to the list before sorting. Sticky.
     #[serde(skip_serializing_if = "Option::is_none")]
