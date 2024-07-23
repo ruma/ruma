89d1634457f9f055afd0ff8898cd8035042e6058
Timo <toger5@hotmail.de>
2024-07-23 12:22:12 +0200

    Future Events: make the future update action authenticated. ( + add tests)

--- crates/ruma-client-api/src/future/update_future.rs
+++ crates/ruma-client-api/src/future/update_future.rs
@@ -11,29 +11,43 @@ pub mod unstable {
         api::{request, response, Metadata},
         metadata,
     };
+    use serde::{Deserialize, Serialize};
 
     const METADATA: Metadata = metadata! {
         method: POST,
         rate_limited: true,
-        authentication: None,
+        authentication: AccessToken,
         history: {
-            unstable => "/_matrix/client/unstable/org.matrix.msc4140/future/:token",
+            unstable => "/_matrix/client/unstable/org.matrix.msc4140/update_future",
         }
     };
 
+    /// The possible update actions we can do for updating a future.
+    #[derive(Serialize, Deserialize, Debug, Clone)]
+    #[serde(rename_all = "lowercase")]
+    pub enum UpdateAction {
+        /// Restart the Future event timeout. (heartbeat ping)
+        Restart,
+        /// Send the Future event immediately independent of the timeout state. (deletes all
+        /// timers)
+        Send,
+        /// Delete the Future event and never send it. (deletes all timers)
+        Cancel,
+    }
     /// Request type for the [`update_future`](crate::future::update_future) endpoint.
     #[request(error = crate::Error)]
     pub struct Request {
-        /// The token.
-        #[ruma_api(path)]
-        pub token: String,
+        /// The future if that we want to update.
+        pub future_id: String,
+        /// Which kind of update we want to request for the Future event.
+        pub action: UpdateAction,
     }
 
     impl Request {
         /// Creates a new `Request` to update a future. This is an unauthenticated request and only
         /// requires the future token.
-        pub fn new(token: String) -> serde_json::Result<Self> {
-            Ok(Self { token })
+        pub fn new(future_id: String, action: UpdateAction) -> serde_json::Result<Self> {
+            Ok(Self { future_id, action })
         }
     }
 
@@ -44,6 +58,38 @@ pub mod unstable {
         /// Creates a new response for the [`update_future`](crate::future::update_future) endpoint.
         pub fn new() -> Self {
             Response {}
+        }
+    }
+
+    #[cfg(all(test, feature = "client"))]
+    mod tests {
+        use ruma_common::api::{MatrixVersion, OutgoingRequest, SendAccessToken};
+        use serde_json::{json, Value as JsonValue};
+
+        use super::{Request, UpdateAction};
+        #[test]
+        fn serialize_update_future_request() {
+            let request: http::Request<Vec<u8>> =
+                Request::new("1234".to_owned(), UpdateAction::Cancel)
+                    .unwrap()
+                    .try_into_http_request(
+                        "https://homeserver.tld",
+                        SendAccessToken::IfRequired("auth_tok"),
+                        &[MatrixVersion::V1_1],
+                    )
+                    .unwrap();
+
+            let (parts, body) = request.into_parts();
+
+            assert_eq!(
+                "https://homeserver.tld/_matrix/client/unstable/org.matrix.msc4140/update_future",
+                parts.uri.to_string()
+            );
+            assert_eq!("POST", parts.method.to_string());
+            assert_eq!(
+                json!({"action": "cancel", "future_id":"1234"}),
+                serde_json::from_str::<JsonValue>(std::str::from_utf8(&body).unwrap()).unwrap()
+            );
         }
     }
 }
