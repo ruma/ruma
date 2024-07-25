00b5ac0089ae63f7b49397e4b3fa6ae12c2af0bf
Timo <toger5@hotmail.de>
2024-07-25 16:36:11 +0200

    Future Events: Add unstable prefix to delay query parameter.

--- crates/ruma-client-api/src/future.rs
+++ crates/ruma-client-api/src/future.rs
@@ -8,35 +8,29 @@ use serde::{Deserialize, Serialize};
 use web_time::Duration;
 
 /// The query parameters for a future request.
-/// It can contain the possible timeout and the future_parent_id combinations.
+/// It can contain the possible `timeout` and `future_parent_id` combinations.
+///
+/// This is an Enum since the following properties might be added:
+///
+/// The **Timeout** case might get an additional `future_parent_id` property.
+/// The optional parent id is used to create a secondary timeout.  
+/// In a future group with two timeouts only one of them will ever be sent.
+///
+/// The **Action** case might be added:
+/// Adds an additional action to a future without a timeout but requires a future group_id.
+/// A possible matrix event that this future group can resolve to. It can be sent by using the
+/// send_token as an alternative to the timeout future of an already existing group.
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 #[cfg_attr(not(feature = "unstable-exhaustive-types"), non_exhaustive)]
 #[serde(untagged)]
 pub enum FutureParameters {
     /// Only sending the `delay` creates a timeout future with a new (server generated)
-    /// future id (delay_id). The optional group id is used to create a secondary timeout.  
-    /// In a future group with two timeouts only one of them will ever be sent.
+    /// future id (delay_id).
     Timeout {
         /// The timeout duration for this Future.
         #[serde(with = "ruma_common::serde::duration::ms")]
-        #[serde(rename = "delay")]
+        #[serde(rename = "org.matrix.msc4140.delay")]
         timeout: Duration,
-        /// The associated group for this Future.
-        ///
-        /// Currently Unused.
-        #[serde(skip_serializing_if = "Option::is_none")]
-        #[serde(rename = "delay_parent_id")]
-        future_parent_id: Option<String>,
-    },
-
-    /// Adds an additional action to a future without a timeout but requires a future group_id.
-    /// A possible matrix event that this future group can resolve to. It can be sent by using the
-    /// send_token as an alternative to the timeout future of an already existing group.
-    ///
-    /// Currently unused.
-    Action {
-        /// The associated group for this Future.
-        #[serde(rename = "delay_parent_id")]
-        future_parent_id: String,
     },
 }
