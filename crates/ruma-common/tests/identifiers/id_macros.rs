70518920779792e30cf150a9a0794579b37d7dd9
Jonas Platte <jplatte+git@posteo.de>
2024-09-10 21:22:02 +0200

    Move integration tests one directory level deeper
    … and rename from tests to it (integration tests).
    This allows enabling the mod_module_files lint everywhere again.

--- crates/ruma-common/tests/identifiers/id_macros.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-fn ui() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/identifiers/ui/01-valid-id-macros.rs");
-    t.compile_fail("tests/identifiers/ui/02-invalid-id-macros.rs");
-    t.compile_fail("tests/identifiers/ui/03-invalid-new-id-macros.rs");
-}
