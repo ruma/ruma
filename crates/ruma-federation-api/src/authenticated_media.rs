bc976b0f1cc2e9fc78c02f6031ef0530f55e5640
Matthias Ahouansou <matthias@ahouansou.cz>
2024-09-30 21:37:13 +0100

    federation-api: implement MSC2702

--- crates/ruma-federation-api/src/authenticated_media.rs
+++ crates/ruma-federation-api/src/authenticated_media.rs
@@ -55,8 +55,16 @@ pub struct Content {
 
 impl Content {
     /// Creates a new `Content` with the given bytes.
-    pub fn new(file: Vec<u8>) -> Self {
-        Self { file, content_type: None, content_disposition: None }
+    pub fn new(
+        file: Vec<u8>,
+        content_type: String,
+        content_disposition: ContentDisposition,
+    ) -> Self {
+        Self {
+            file,
+            content_type: Some(content_type),
+            content_disposition: Some(content_disposition),
+        }
     }
 }
 
