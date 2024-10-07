2c845c27fb2a1eee5365f95c7197b60182892a11
Kévin Commaille <zecakeh@tedomum.fr>
2024-10-07 22:25:26 +0200

    api: Add MatrixVersion::V1_12

--- crates/ruma-common/src/api/metadata.rs
+++ crates/ruma-common/src/api/metadata.rs
@@ -546,6 +546,11 @@ pub enum MatrixVersion {
     ///
     /// See <https://spec.matrix.org/v1.11/>.
     V1_11,
+
+    /// Version 1.12 of the Matrix specification, released in Q3 2024.
+    ///
+    /// See <https://spec.matrix.org/v1.12/>.
+    V1_12,
 }
 
 impl TryFrom<&str> for MatrixVersion {
@@ -570,6 +575,7 @@ impl TryFrom<&str> for MatrixVersion {
             "v1.9" => V1_9,
             "v1.10" => V1_10,
             "v1.11" => V1_11,
+            "v1.12" => V1_12,
             _ => return Err(UnknownVersionError),
         })
     }
@@ -620,6 +626,7 @@ impl MatrixVersion {
             MatrixVersion::V1_9 => (1, 9),
             MatrixVersion::V1_10 => (1, 10),
             MatrixVersion::V1_11 => (1, 11),
+            MatrixVersion::V1_12 => (1, 12),
         }
     }
 
@@ -638,6 +645,7 @@ impl MatrixVersion {
             (1, 9) => Ok(MatrixVersion::V1_9),
             (1, 10) => Ok(MatrixVersion::V1_10),
             (1, 11) => Ok(MatrixVersion::V1_11),
+            (1, 12) => Ok(MatrixVersion::V1_12),
             _ => Err(UnknownVersionError),
         }
     }
@@ -734,7 +742,9 @@ impl MatrixVersion {
             // <https://spec.matrix.org/v1.10/rooms/#complete-list-of-room-versions>
             | MatrixVersion::V1_10
             // <https://spec.matrix.org/v1.11/rooms/#complete-list-of-room-versions>
-            | MatrixVersion::V1_11  => RoomVersionId::V10,
+            | MatrixVersion::V1_11
+            // <https://spec.matrix.org/v1.12/rooms/#complete-list-of-room-versions>
+            | MatrixVersion::V1_12  => RoomVersionId::V10,
         }
     }
 }
