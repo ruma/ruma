5f337744aa3149b3ec5f0c9fae63633f7915324e
Charles Hall <charles@computer.surgery>
2024-11-06 12:47:51 -0800

    state-res: test origin_server_ts tiebreaking
    You may notice that the `m.room.join_rules` in the resolved state is
    incorrect. This will be fixed in the next commit.

--- crates/ruma-state-res/tests/it/resolve/snapshot_tests.rs
+++ crates/ruma-state-res/tests/it/resolve/snapshot_tests.rs
@@ -3,3 +3,13 @@
 // Test the minimal set of events required to create a room with the
 // "private_chat" preset.
 snapshot_test!(minimal_private_chat, ["bootstrap-private-chat.json"]);
+
+// Start with a private room, then transition its join rules to restricted, then
+// to public. The events in the second file are tied topologically, so they must
+// have the tiebreaking algorithm applied. The ordering should be decided by
+// the `origin_server_ts` fields of these events, not the `event_id` fields. The
+// power levels of these events are equivalent, so they don't really matter.
+snapshot_test!(
+    origin_server_ts_tiebreak,
+    ["bootstrap-private-chat.json", "origin-server-ts-tiebreak.json"],
+);
