8bf84529d59553d45272465115d1e306af3977f0
Mathieu Velten <mathieu.velten@beta.gouv.fr>
2024-11-12 15:34:26 +0100

    Address comments

--- crates/ruma-events/src/direct.rs
+++ crates/ruma-events/src/direct.rs
@@ -21,14 +21,14 @@ use serde::{Deserialize, Serialize};
 pub struct DirectUserIdentifier(str);
 
 impl DirectUserIdentifier {
-    /// Get this `DirectUserIdentifier` as an &[`UserId`] if it is one.
+    /// Get this `DirectUserIdentifier` as an [`UserId`] if it is one.
     pub fn as_user_id(&self) -> Option<&UserId> {
         self.0.try_into().ok()
     }
 }
 
 impl OwnedDirectUserIdentifier {
-    /// Get this `OwnedDirectUserIdentifier` as an &[`UserId`] if it is one.
+    /// Get this `OwnedDirectUserIdentifier` as an [`UserId`] if it is one.
     pub fn as_user_id(&self) -> Option<&UserId> {
         self.0.try_into().ok()
     }
@@ -97,25 +97,49 @@ impl<'a> From<&'a UserId> for &'a DirectUserIdentifier {
 
 impl PartialEq<&UserId> for &DirectUserIdentifier {
     fn eq(&self, other: &&UserId) -> bool {
-        &self.0 == other.as_str()
+        self.0.eq(other.as_str())
+    }
+}
+
+impl PartialEq<&DirectUserIdentifier> for &UserId {
+    fn eq(&self, other: &&DirectUserIdentifier) -> bool {
+        other.0.eq(self.as_str())
     }
 }
 
 impl PartialEq<OwnedUserId> for &DirectUserIdentifier {
     fn eq(&self, other: &OwnedUserId) -> bool {
-        &self.0 == other.as_str()
+        self.0.eq(other.as_str())
+    }
+}
+
+impl PartialEq<&DirectUserIdentifier> for OwnedUserId {
+    fn eq(&self, other: &&DirectUserIdentifier) -> bool {
+        other.0.eq(self.as_str())
     }
 }
 
 impl PartialEq<&UserId> for OwnedDirectUserIdentifier {
     fn eq(&self, other: &&UserId) -> bool {
-        &self.0 == other.as_str()
+        self.0.eq(other.as_str())
+    }
+}
+
+impl PartialEq<OwnedDirectUserIdentifier> for &UserId {
+    fn eq(&self, other: &OwnedDirectUserIdentifier) -> bool {
+        other.0.eq(self.as_str())
     }
 }
 
 impl PartialEq<OwnedUserId> for OwnedDirectUserIdentifier {
     fn eq(&self, other: &OwnedUserId) -> bool {
-        &self.0 == other.as_str()
+        self.0.eq(other.as_str())
+    }
+}
+
+impl PartialEq<OwnedDirectUserIdentifier> for OwnedUserId {
+    fn eq(&self, other: &OwnedDirectUserIdentifier) -> bool {
+        other.0.eq(self.as_str())
     }
 }
 
@@ -166,7 +190,7 @@ impl FromIterator<(OwnedDirectUserIdentifier, Vec<OwnedRoomId>)> for DirectEvent
 mod tests {
     use std::collections::BTreeMap;
 
-    use ruma_common::{owned_room_id, owned_user_id, user_id, OwnedUserId};
+    use ruma_common::{owned_room_id, user_id, OwnedUserId};
     // use ruma_macros::user_id;
     use serde_json::{from_value as from_json_value, json, to_value as to_json_value};
 
@@ -176,12 +200,12 @@ mod tests {
     #[test]
     fn serialization() {
         let mut content = DirectEventContent(BTreeMap::new());
-        let alice = owned_user_id!("@alice:ruma.io");
+        let alice = user_id!("@alice:ruma.io");
         let alice_mail = "alice@ruma.io";
         let rooms = vec![owned_room_id!("!1:ruma.io")];
         let mail_rooms = vec![owned_room_id!("!3:ruma.io")];
 
-        content.insert(alice.clone().into(), rooms.clone());
+        content.insert(alice.into(), rooms.clone());
         content.insert(alice_mail.into(), mail_rooms.clone());
 
         let json_data = json!({
@@ -194,14 +218,14 @@ mod tests {
 
     #[test]
     fn deserialization() {
-        let alice = owned_user_id!("@alice:ruma.io");
+        let alice = user_id!("@alice:ruma.io");
         let alice_mail = "alice@ruma.io";
         let rooms = vec![owned_room_id!("!1:ruma.io"), owned_room_id!("!2:ruma.io")];
         let mail_rooms = vec![owned_room_id!("!3:ruma.io")];
 
         let json_data = json!({
             "content": {
-                alice.clone(): rooms,
+                alice: rooms,
                 alice_mail: mail_rooms,
             },
             "type": "m.direct"
@@ -209,12 +233,12 @@ mod tests {
 
         let event: DirectEvent = from_json_value(json_data).unwrap();
 
-        let direct_rooms = event.content.get(&OwnedDirectUserIdentifier::from(alice)).unwrap();
+        let direct_rooms = event.content.get(<&DirectUserIdentifier>::from(alice)).unwrap();
         assert!(direct_rooms.contains(&rooms[0]));
         assert!(direct_rooms.contains(&rooms[1]));
 
         let email_direct_rooms =
-            event.content.get(&OwnedDirectUserIdentifier::from(alice_mail)).unwrap();
+            event.content.get(<&DirectUserIdentifier>::from(alice_mail)).unwrap();
         assert!(email_direct_rooms.contains(&mail_rooms[0]));
     }
 
@@ -234,9 +258,15 @@ mod tests {
         let alice_user_id = user_id!("@alice:ruma.io");
         let alice_direct_uid_mail: &DirectUserIdentifier = alice_user_id.into();
         assert_eq!(alice_direct_uid_mail, alice_user_id);
+        assert_eq!(alice_direct_uid_mail, alice_user_id.to_owned());
+        assert_eq!(alice_user_id, alice_direct_uid_mail);
+        assert_eq!(alice_user_id.to_owned(), alice_direct_uid_mail);
 
         let alice_user_id = user_id!("@alice:ruma.io");
         let alice_direct_uid_mail: OwnedDirectUserIdentifier = alice_user_id.into();
         assert_eq!(alice_direct_uid_mail, alice_user_id);
+        assert_eq!(alice_direct_uid_mail, alice_user_id.to_owned());
+        assert_eq!(alice_user_id, alice_direct_uid_mail);
+        assert_eq!(alice_user_id.to_owned(), alice_direct_uid_mail);
     }
 }
