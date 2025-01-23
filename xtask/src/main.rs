12461a63c7b4ab12666beaa3b0a9b5134c9731ff
Jonas Platte <jplatte+git@posteo.de>
2025-01-23 23:45:15 +0100

    Bump nightly toolchain

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -15,7 +15,7 @@ use serde::Deserialize;
 use serde_json::from_str as from_json_str;
 
 // Keep in sync with version in `rust-toolchain.toml` and `.github/workflows/ci.yml`
-const NIGHTLY: &str = "nightly-2024-11-26";
+const NIGHTLY: &str = "nightly-2025-01-23";
 
 mod cargo;
 mod ci;
