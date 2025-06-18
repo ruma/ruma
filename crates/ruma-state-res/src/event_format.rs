fc600f764f9a922bea55fc613b545b3aae5d26c2
Kévin Commaille <zecakeh@tedomum.fr>
2025-06-18 16:24:37 +0200

    state-res: Add tests for check_pdu_format

--- crates/ruma-state-res/src/event_format.rs
+++ crates/ruma-state-res/src/event_format.rs
@@ -154,3 +154,176 @@ fn extract_array_field<'a>(
         None => Ok(None),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::iter::repeat_n;
+
+    use js_int::int;
+    use ruma_common::{
+        room_version_rules::EventFormatRules, CanonicalJsonObject, CanonicalJsonValue,
+    };
+    use serde_json::{from_value as from_json_value, json};
+
+    use super::check_pdu_format;
+
+    /// Construct a PDU valid for the event format of room v1.
+    fn pdu_v1() -> CanonicalJsonObject {
+        let pdu = json!({
+            "auth_events": [
+                [
+                    "$af232176:example.org",
+                    { "sha256": "abase64encodedsha256hashshouldbe43byteslong" },
+                ],
+            ],
+            "content": {
+                "key": "value",
+            },
+            "depth": 12,
+            "event_id": "$a4ecee13e2accdadf56c1025:example.com",
+            "hashes": {
+                "sha256": "thishashcoversallfieldsincasethisisredacted"
+            },
+            "origin_server_ts": 1_838_188_000,
+            "prev_events": [
+                [
+                    "$af232176:example.org",
+                    { "sha256": "abase64encodedsha256hashshouldbe43byteslong" }
+                ],
+            ],
+            "room_id": "!UcYsUzyxTGDxLBEvLy:example.org",
+            "sender": "@alice:example.com",
+            "signatures": {
+                "example.com": {
+                    "ed25519:key_version": "these86bytesofbase64signaturecoveressentialfieldsincludinghashessocancheckredactedpdus",
+                },
+            },
+            "type": "m.room.message",
+            "unsigned": {
+                "age": 4612,
+            },
+        });
+        from_json_value(pdu).unwrap()
+    }
+
+    /// Construct a PDU valid for the event format of room v3.
+    fn pdu_v3() -> CanonicalJsonObject {
+        let pdu = json!({
+            "auth_events": [
+                "$base64encodedeventid",
+                "$adifferenteventid",
+            ],
+            "content": {
+                "key": "value",
+            },
+            "depth": 12,
+            "hashes": {
+                "sha256": "thishashcoversallfieldsincasethisisredacted",
+            },
+            "origin_server_ts": 1_838_188_000,
+            "prev_events": [
+                "$base64encodedeventid",
+                "$adifferenteventid",
+            ],
+            "redacts": "$some/old+event",
+            "room_id": "!UcYsUzyxTGDxLBEvLy:example.org",
+            "sender": "@alice:example.com",
+            "signatures": {
+                "example.com": {
+                    "ed25519:key_version": "these86bytesofbase64signaturecoveressentialfieldsincludinghashessocancheckredactedpdus",
+                },
+            },
+            "type": "m.room.message",
+            "unsigned": {
+                "age": 4612,
+            }
+        });
+        from_json_value(pdu).unwrap()
+    }
+
+    #[test]
+    fn check_pdu_format_valid_v1() {
+        check_pdu_format(&pdu_v1(), &EventFormatRules::V1).unwrap();
+    }
+
+    #[test]
+    fn check_pdu_format_valid_v3() {
+        check_pdu_format(&pdu_v3(), &EventFormatRules::V3).unwrap();
+    }
+
+    #[test]
+    fn check_pdu_format_pdu_too_big() {
+        // Add a lot of data in the content to reach MAX_PDU_SIZE.
+        let mut pdu = pdu_v3();
+        let content = pdu.get_mut("content").unwrap().as_object_mut().unwrap();
+        let long_string = repeat_n('a', 66_000).collect::<String>();
+        content.insert("big_data".to_owned(), long_string.into());
+
+        check_pdu_format(&pdu, &EventFormatRules::V3).unwrap_err();
+    }
+
+    #[test]
+    fn check_pdu_format_fields_missing() {
+        for field in
+            &["event_id", "sender", "room_id", "type", "prev_events", "auth_events", "depth"]
+        {
+            let mut pdu = pdu_v1();
+            pdu.remove(*field).unwrap();
+
+            check_pdu_format(&pdu, &EventFormatRules::V1).unwrap_err();
+        }
+    }
+
+    #[test]
+    fn check_pdu_format_strings_too_big() {
+        for field in &["event_id", "sender", "room_id", "type", "state_key"] {
+            let mut pdu = pdu_v1();
+            let value = repeat_n('a', 300).collect::<String>();
+            pdu.insert((*field).to_owned(), value.into());
+            check_pdu_format(&pdu, &EventFormatRules::V1).unwrap_err();
+        }
+    }
+
+    #[test]
+    fn check_pdu_format_strings_wrong_format() {
+        for field in &["event_id", "sender", "room_id", "type", "state_key"] {
+            let mut pdu = pdu_v1();
+            pdu.insert((*field).to_owned(), true.into());
+            check_pdu_format(&pdu, &EventFormatRules::V1).unwrap_err();
+        }
+    }
+
+    #[test]
+    fn check_pdu_format_arrays_too_big() {
+        for field in &["prev_events", "auth_events"] {
+            let mut pdu = pdu_v3();
+            let value =
+                repeat_n(CanonicalJsonValue::from("$eventid".to_owned()), 30).collect::<Vec<_>>();
+            pdu.insert((*field).to_owned(), value.into());
+            check_pdu_format(&pdu, &EventFormatRules::V3).unwrap_err();
+        }
+    }
+
+    #[test]
+    fn check_pdu_format_arrays_wrong_format() {
+        for field in &["prev_events", "auth_events"] {
+            let mut pdu = pdu_v3();
+            pdu.insert((*field).to_owned(), true.into());
+            check_pdu_format(&pdu, &EventFormatRules::V3).unwrap_err();
+        }
+    }
+
+    #[test]
+    fn check_pdu_format_negative_depth() {
+        let mut pdu = pdu_v3();
+        pdu.insert("depth".to_owned(), int!(-1).into()).unwrap();
+        check_pdu_format(&pdu, &EventFormatRules::V3).unwrap_err();
+    }
+
+    #[test]
+    fn check_pdu_format_depth_wrong_format() {
+        let mut pdu = pdu_v3();
+        pdu.insert("depth".to_owned(), true.into());
+        check_pdu_format(&pdu, &EventFormatRules::V3).unwrap_err();
+    }
+}
