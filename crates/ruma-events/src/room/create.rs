0dde591a2b2d5ec4b90f49a56abf9d21abd0b465
Kévin Commaille <zecakeh@tedomum.fr>
2025-06-27 15:04:05 +0200

    events: Deprecate and make optional `event_id` field of PreviousRoom
    Clients must not rely on it anymore, but servers should send it when
    possible for backwards-compatibility.

--- crates/ruma-events/src/room/create.rs
+++ crates/ruma-events/src/room/create.rs
@@ -124,13 +124,18 @@ pub struct PreviousRoom {
     pub room_id: OwnedRoomId,
 
     /// The event ID of the last known event in the old room.
-    pub event_id: OwnedEventId,
+    #[deprecated = "\
+        This field should be sent by servers when possible for backwards compatibility \
+        but clients should not rely on it."]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub event_id: Option<OwnedEventId>,
 }
 
 impl PreviousRoom {
-    /// Creates a new `PreviousRoom` from the given room and event IDs.
-    pub fn new(room_id: OwnedRoomId, event_id: OwnedEventId) -> Self {
-        Self { room_id, event_id }
+    /// Creates a new `PreviousRoom` from the given room ID.
+    pub fn new(room_id: OwnedRoomId) -> Self {
+        #[allow(deprecated)]
+        Self { room_id, event_id: None }
     }
 }
 
