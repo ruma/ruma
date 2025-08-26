2f3d21cb75768e120ca4c3ecf8cc9e0157fdb182
Timo K <toger5@hotmail.de>
2025-08-26 15:56:55 +0200

    Add deprecation macro to `call.notify` event
    Signed-off-by: Timo K <toger5@hotmail.de>

--- crates/ruma-events/src/call.rs
+++ crates/ruma-events/src/call.rs
@@ -10,6 +10,7 @@ pub mod invite;
 pub mod member;
 pub mod negotiate;
 #[cfg(feature = "unstable-msc4075")]
+#[deprecated = "Since Matrix 0.30.6. Use the m.rtc.notification event instead."]
 pub mod notify;
 pub mod reject;
 pub mod sdp_stream_metadata_changed;
