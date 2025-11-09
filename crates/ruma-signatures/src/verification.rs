422c038e2a3d220730758935ae5211c5e8000c21
Jonas Platte <jplatte+git@posteo.de>
2025-11-09 19:26:47 +0100

    signatures: Update to Rust 2024

--- crates/ruma-signatures/src/verification.rs
+++ crates/ruma-signatures/src/verification.rs
@@ -65,7 +65,9 @@ pub enum Verified {
 }
 
 /// Get the verifier for the given algorithm, if it is supported.
-pub(crate) fn verifier_from_algorithm(algorithm: &SigningKeyAlgorithm) -> Option<impl Verifier> {
+pub(crate) fn verifier_from_algorithm(
+    algorithm: &SigningKeyAlgorithm,
+) -> Option<impl Verifier + use<>> {
     match algorithm {
         SigningKeyAlgorithm::Ed25519 => Some(Ed25519Verifier),
         _ => None,
