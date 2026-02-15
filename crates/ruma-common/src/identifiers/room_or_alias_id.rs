e802511f367e45c2f706906fbd89d1b961a4e5cc
nextn <nextn320@gmail.com>
2026-02-15 23:11:16 +0630

    identifiers: use Arc raw path for RoomOrAliasId conversions

--- crates/ruma-common/src/identifiers/room_or_alias_id.rs
+++ crates/ruma-common/src/identifiers/room_or_alias_id.rs
@@ -92,17 +92,31 @@ impl<'a> From<&'a RoomAliasId> for &'a RoomOrAliasId {
 
 impl From<OwnedRoomId> for OwnedRoomOrAliasId {
     fn from(room_id: OwnedRoomId) -> Self {
-        // FIXME: Arc storage still allocates here.
-        let room_id: Box<RoomId> = room_id.into();
-        RoomOrAliasId::from_box(room_id.into()).into()
+        #[cfg(not(any(ruma_identifiers_storage = "Arc")))]
+        {
+            let room_id: Box<RoomId> = room_id.into();
+            RoomOrAliasId::from_box(room_id.into()).into()
+        }
+
+        #[cfg(ruma_identifiers_storage = "Arc")]
+        {
+            unsafe { Self::from_raw(room_id.into_raw() as *const RoomOrAliasId) }
+        }
     }
 }
 
 impl From<OwnedRoomAliasId> for OwnedRoomOrAliasId {
     fn from(room_alias_id: OwnedRoomAliasId) -> Self {
-        // FIXME: Arc storage still allocates here.
-        let room_alias_id: Box<RoomAliasId> = room_alias_id.into();
-        RoomOrAliasId::from_box(room_alias_id.into()).into()
+        #[cfg(not(any(ruma_identifiers_storage = "Arc")))]
+        {
+            let room_alias_id: Box<RoomAliasId> = room_alias_id.into();
+            RoomOrAliasId::from_box(room_alias_id.into()).into()
+        }
+
+        #[cfg(ruma_identifiers_storage = "Arc")]
+        {
+            unsafe { Self::from_raw(room_alias_id.into_raw() as *const RoomOrAliasId) }
+        }
     }
 }
 
@@ -132,13 +146,28 @@ impl TryFrom<OwnedRoomOrAliasId> for OwnedRoomId {
     type Error = OwnedRoomAliasId;
 
     fn try_from(id: OwnedRoomOrAliasId) -> Result<OwnedRoomId, OwnedRoomAliasId> {
-        // FIXME: Arc storage still allocates here.
-        let variant = id.variant();
-        let id: Box<RoomOrAliasId> = id.into();
+        #[cfg(not(any(ruma_identifiers_storage = "Arc")))]
+        {
+            let variant = id.variant();
+            let id: Box<RoomOrAliasId> = id.into();
 
-        match variant {
-            Variant::RoomId => Ok(RoomId::from_box(id.into()).into()),
-            Variant::RoomAliasId => Err(RoomAliasId::from_box(id.into()).into()),
+            match variant {
+                Variant::RoomId => Ok(RoomId::from_box(id.into()).into()),
+                Variant::RoomAliasId => Err(RoomAliasId::from_box(id.into()).into()),
+            }
+        }
+
+        #[cfg(ruma_identifiers_storage = "Arc")]
+        {
+            let variant = id.variant();
+            let ptr = id.into_raw();
+
+            unsafe {
+                match variant {
+                    Variant::RoomId => Ok(Self::from_raw(ptr as *const RoomId)),
+                    Variant::RoomAliasId => Err(OwnedRoomAliasId::from_raw(ptr as *const RoomAliasId)),
+                }
+            }
         }
     }
 }
@@ -147,13 +176,28 @@ impl TryFrom<OwnedRoomOrAliasId> for OwnedRoomAliasId {
     type Error = OwnedRoomId;
 
     fn try_from(id: OwnedRoomOrAliasId) -> Result<OwnedRoomAliasId, OwnedRoomId> {
-        // FIXME: Arc storage still allocates here.
-        let variant = id.variant();
-        let id: Box<RoomOrAliasId> = id.into();
+        #[cfg(not(any(ruma_identifiers_storage = "Arc")))]
+        {
+            let variant = id.variant();
+            let id: Box<RoomOrAliasId> = id.into();
 
-        match variant {
-            Variant::RoomAliasId => Ok(RoomAliasId::from_box(id.into()).into()),
-            Variant::RoomId => Err(RoomId::from_box(id.into()).into()),
+            match variant {
+                Variant::RoomAliasId => Ok(RoomAliasId::from_box(id.into()).into()),
+                Variant::RoomId => Err(RoomId::from_box(id.into()).into()),
+            }
+        }
+
+        #[cfg(ruma_identifiers_storage = "Arc")]
+        {
+            let variant = id.variant();
+            let ptr = id.into_raw();
+
+            unsafe {
+                match variant {
+                    Variant::RoomAliasId => Ok(Self::from_raw(ptr as *const RoomAliasId)),
+                    Variant::RoomId => Err(OwnedRoomId::from_raw(ptr as *const RoomId)),
+                }
+            }
         }
     }
 }
