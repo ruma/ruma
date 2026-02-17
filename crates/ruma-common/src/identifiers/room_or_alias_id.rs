c039fcfff0f924b35b842d477c91a897e5a0c149
nextn <nextn320@gmail.com>
2026-02-17 22:46:06 +0630

    common: format room_or_alias_id conversion match arm

--- crates/ruma-common/src/identifiers/room_or_alias_id.rs
+++ crates/ruma-common/src/identifiers/room_or_alias_id.rs
@@ -134,9 +134,7 @@ impl TryFrom<OwnedRoomOrAliasId> for OwnedRoomId {
         unsafe {
             match variant {
                 Variant::RoomId => Ok(Self::from_raw(ptr as *const RoomId)),
-                Variant::RoomAliasId => {
-                    Err(OwnedRoomAliasId::from_raw(ptr as *const RoomAliasId))
-                }
+                Variant::RoomAliasId => Err(OwnedRoomAliasId::from_raw(ptr as *const RoomAliasId)),
             }
         }
     }
