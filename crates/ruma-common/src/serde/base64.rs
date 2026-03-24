b47f02cab28cc56aa13610ab7ed441732f0f710f
Kévin Commaille <zecakeh@tedomum.fr>
2026-03-24 10:34:24 +0100

    serde: Improvements to work with arrays in Base64
    Add support for `Base64::parse()` when `B` is an array. This allows to
    enforce the size of the decoded bytes when parsing a string or
    deserializing.
    
    This also adds a way to access the inner bytes without consuming the
    wrapper or exposing it as a slice, to be able to keep the length bound.

--- crates/ruma-common/src/serde/base64.rs
+++ crates/ruma-common/src/serde/base64.rs
@@ -91,6 +91,11 @@ impl<C: Base64Config, B: AsRef<[u8]>> Base64<C, B> {
 }
 
 impl<C, B> Base64<C, B> {
+    /// Get a reference to the raw bytes held by this `Base64` instance.
+    pub fn as_inner(&self) -> &B {
+        &self.bytes
+    }
+
     /// Get the raw bytes held by this `Base64` instance.
     pub fn into_inner(self) -> B {
         self.bytes
@@ -102,10 +107,13 @@ impl<C: Base64Config> Base64<C> {
     pub fn empty() -> Self {
         Self::new(Vec::new())
     }
+}
 
+impl<C: Base64Config, B: TryFromBase64DecodedBytes> Base64<C, B> {
     /// Parse some base64-encoded data to create a `Base64` instance.
     pub fn parse(encoded: impl AsRef<[u8]>) -> Result<Self, Base64DecodeError> {
-        Self::ENGINE.decode(encoded).map(Self::new).map_err(Base64DecodeError)
+        let decoded = Self::ENGINE.decode(encoded).map_err(Base64DecodeError::base64)?;
+        B::try_from_bytes(decoded).map(Self::new)
     }
 }
 
@@ -121,7 +129,7 @@ impl<C: Base64Config, B: AsRef<[u8]>> fmt::Display for Base64<C, B> {
     }
 }
 
-impl<'de, C: Base64Config> Deserialize<'de> for Base64<C> {
+impl<'de, C: Base64Config, B: TryFromBase64DecodedBytes> Deserialize<'de> for Base64<C, B> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: Deserializer<'de>,
@@ -140,9 +148,45 @@ impl<C: Base64Config, B: AsRef<[u8]>> Serialize for Base64<C, B> {
     }
 }
 
+/// Marker trait for indicating which inner `B` "bytes" type can be converted from decoded base64
+/// bytes.
+///
+/// This is used as a bound in [`Base64::parse()`] to provide a more helpful error message than
+/// using a `TryFrom<Vec<u8>>` implementation.
+pub trait TryFromBase64DecodedBytes: Sized + AsRef<[u8]> {
+    /// Convert the given bytes to this type.
+    #[doc(hidden)]
+    fn try_from_bytes(bytes: Vec<u8>) -> Result<Self, Base64DecodeError>;
+}
+
+impl TryFromBase64DecodedBytes for Vec<u8> {
+    fn try_from_bytes(bytes: Vec<u8>) -> Result<Self, Base64DecodeError> {
+        Ok(bytes)
+    }
+}
+
+impl<const N: usize> TryFromBase64DecodedBytes for [u8; N] {
+    fn try_from_bytes(bytes: Vec<u8>) -> Result<Self, Base64DecodeError> {
+        Self::try_from(bytes)
+            .map_err(|bytes| Base64DecodeError::invalid_decoded_length(bytes.len(), N))
+    }
+}
+
 /// An error that occurred while decoding a base64 string.
 #[derive(Clone)]
-pub struct Base64DecodeError(base64::DecodeError);
+pub struct Base64DecodeError(Base64DecodeErrorInner);
+
+impl Base64DecodeError {
+    /// Construct a `Base64DecodeError` from an invalid base64 encoding error.
+    fn base64(error: base64::DecodeError) -> Self {
+        Self(Base64DecodeErrorInner::Base64(error))
+    }
+
+    /// Construct a `Base64DecodeError` from an invalid decoded bytes length error.
+    fn invalid_decoded_length(len: usize, expected: usize) -> Self {
+        Self(Base64DecodeErrorInner::InvalidDecodedLength { len, expected })
+    }
+}
 
 impl fmt::Debug for Base64DecodeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -152,18 +196,38 @@ impl fmt::Debug for Base64DecodeError {
 
 impl fmt::Display for Base64DecodeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        self.0.fmt(f)
+        match &self.0 {
+            Base64DecodeErrorInner::Base64(error) => write!(f, "invalid base64 encoding: {error}"),
+            Base64DecodeErrorInner::InvalidDecodedLength { len, expected } => {
+                write!(f, "invalid decoded base64 bytes length: {len}, expected {expected}")
+            }
+        }
     }
 }
 
 impl std::error::Error for Base64DecodeError {}
+
+/// An error that occurred while decoding a base64 string.
+#[derive(Debug, Clone)]
+enum Base64DecodeErrorInner {
+    /// The base64 encoding is invalid.
+    Base64(base64::DecodeError),
+
+    /// The decoded bytes have the wrong length to fit into an array of fixed length.
+    InvalidDecodedLength {
+        /// The length of the input.
+        len: usize,
+        /// The expected length.
+        expected: usize,
+    },
+}
 
 #[cfg(test)]
 mod tests {
     use super::{Base64, Standard};
 
     #[test]
-    fn slightly_malformed_base64() {
+    fn parse_base64() {
         const INPUT: &str = "3UmJnEIzUr2xWyaUnJg5fXwRybwG5FVC6Gq\
             MHverEUn0ztuIsvVxX89JXX2pvdTsOBbLQx+4TVL02l4Cp5wPCm";
         const INPUT_WITH_PADDING: &str = "im9+knCkMNQNh9o6sbdcZw==";
@@ -171,5 +235,11 @@ mod tests {
         Base64::<Standard>::parse(INPUT).unwrap();
         Base64::<Standard>::parse(INPUT_WITH_PADDING)
             .expect("We should be able to decode padded Base64");
+
+        // Check that we can parse with the correct length.
+        Base64::<Standard, [u8; 32]>::parse(INPUT).unwrap_err();
+        Base64::<Standard, [u8; 64]>::parse(INPUT).unwrap();
+        Base64::<Standard, [u8; 32]>::parse(INPUT_WITH_PADDING).unwrap_err();
+        Base64::<Standard, [u8; 16]>::parse(INPUT_WITH_PADDING).unwrap();
     }
 }
