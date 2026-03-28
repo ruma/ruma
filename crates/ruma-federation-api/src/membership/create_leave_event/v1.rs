eefff0d4a13670b72f06fa9514b8efda6e5dd8e8
Kévin Commaille <zecakeh@tedomum.fr>
2026-03-28 08:53:16 +0100

    Update links to point to the latest version of the spec

--- crates/ruma-federation-api/src/membership/create_leave_event/v1.rs
+++ crates/ruma-federation-api/src/membership/create_leave_event/v1.rs
@@ -1,6 +1,6 @@
 //! `/v1/` ([spec])
 //!
-//! [spec]: https://spec.matrix.org/latest/server-server-api/#put_matrixfederationv1send_leaveroomideventid
+//! [spec]: https://spec.matrix.org/v1.17/server-server-api/#put_matrixfederationv1send_leaveroomideventid
 
 use ruma_common::{
     OwnedEventId, OwnedRoomId,
