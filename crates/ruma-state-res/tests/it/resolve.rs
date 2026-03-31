4083195d12d0b2cf732dc7571a30f734ac093a57
Kévin Commaille <zecakeh@tedomum.fr>
2026-03-31 12:52:03 +0200

    state-res: Move all unit tests of resolve() as snapshot tests
    Allows to get rid of some functions and types in unit tests by reusing
    the ones from the snapshot tests.
    
    Also snapshot fixtures are easier to reuse.

--- crates/ruma-state-res/tests/it/resolve.rs
+++ crates/ruma-state-res/tests/it/resolve.rs
@@ -7,6 +7,9 @@ mod macros;
 // "private_chat" preset.
 snapshot_test_batches!(minimal_private_chat, ["bootstrap-private-chat.json"]);
 
+// A create a room with the "public_chat" preset and bob as an admin.
+snapshot_test_batches!(minimal_public_chat, ["bootstrap-public-chat.json"]);
+
 // Start with a private room, then transition its join rules to restricted, then
 // to public. The events in the second file are tied topologically, so they must
 // have the tiebreaking algorithm applied. The ordering should be decided by
@@ -55,4 +58,62 @@ snapshot_test_state_maps!(
     msc4297_problem_b_state_res_v2_1,
     ["MSC4297-problem-B/state-eve.json", "MSC4297-problem-B/state-zara.json"],
     ["MSC4297-problem-B/pdus-v12.json"],
+);
+
+// Test that a power levels change from bob is superseded by his ban from alice.
+snapshot_test_batches!(
+    ban_vs_power_levels,
+    [
+        "bootstrap-public-chat.json",
+        "ban-vs-power-levels-alice.json",
+        "ban-vs-power-levels-bob.json",
+    ],
+);
+
+// Test that a room topic change from bob is superseded by his demotion from alice.
+snapshot_test_batches!(
+    topic_vs_power_levels,
+    [
+        "bootstrap-public-chat.json",
+        "topic-vs-power-levels-alice.json",
+        "topic-vs-power-levels-bob.json",
+    ],
+);
+
+// Test that a power levels change from bob is superseded by his demotion from alice.
+snapshot_test_batches!(
+    power_levels_admin_vs_mod,
+    [
+        "bootstrap-public-chat.json",
+        "power-levels-admin-vs-mod-alice.json",
+        "power-levels-admin-vs-mod-bob.json",
+    ],
+);
+
+// Test that a room topic change from bob is superseded by his ban from alice.
+snapshot_test_batches!(
+    topic_vs_ban,
+    [
+        "bootstrap-public-chat.json",
+        "topic-vs-ban-common.json",
+        "topic-vs-ban-alice.json",
+        "topic-vs-ban-bob.json",
+    ],
+);
+
+// Test that a join from ella is superseded by a join rules change from alice.
+snapshot_test_batches!(
+    join_rules_vs_join,
+    [
+        "bootstrap-public-chat.json",
+        "join-rules-vs-join-common.json",
+        "join-rules-vs-join-alice.json",
+        "join-rules-vs-join-ella.json",
+    ],
+);
+
+// Test that concurrent joins both end up in the state.
+snapshot_test_batches!(
+    concurrent_joins,
+    ["bootstrap-public-chat.json", "concurrent-joins-charlie.json", "concurrent-joins-ella.json",],
 );
