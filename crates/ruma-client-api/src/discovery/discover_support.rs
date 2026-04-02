0478b11724d3303696c068bf826714d8f598615b
Logan Devine <logan@zirco.dev>
2026-04-02 10:40:21 -0700

    add support for experimental MSC4439 in support file

--- crates/ruma-client-api/src/discovery/discover_support.rs
+++ crates/ruma-client-api/src/discovery/discover_support.rs
@@ -84,17 +84,36 @@ pub struct Contact {
     /// At least one of `matrix_id` or `email_address` is required.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub matrix_id: Option<OwnedUserId>,
+
+    /// An optional URI leading to a PGP key that may be used to encrypt messages sent to the
+    /// contact. See MSC4439 for details.
+    #[cfg(feature = "unstable-msc4439")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "dev.zirco.msc4439.pgp_key")]
+    pub pgp_key: Option<String>,
 }
 
 impl Contact {
     /// Creates a new `Contact` with the given role and email address.
     pub fn with_email_address(role: ContactRole, email_address: String) -> Self {
-        Self { role, email_address: Some(email_address), matrix_id: None }
+        Self {
+            role,
+            email_address: Some(email_address),
+            matrix_id: None,
+            #[cfg(feature = "unstable-msc4439")]
+            pgp_key: None,
+        }
     }
 
     /// Creates a new `Contact` with the given role and Matrix User ID.
     pub fn with_matrix_id(role: ContactRole, matrix_id: OwnedUserId) -> Self {
-        Self { role, email_address: None, matrix_id: Some(matrix_id) }
+        Self {
+            role,
+            email_address: None,
+            matrix_id: Some(matrix_id),
+            #[cfg(feature = "unstable-msc4439")]
+            pgp_key: None,
+        }
     }
 }
 
