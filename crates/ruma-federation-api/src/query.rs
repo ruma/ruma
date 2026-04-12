a9469a99e4518bb733b38d0ee5ffd152bcd237c3
Ginger <ginger@gingershaped.computer>
2026-04-12 12:18:00 -0400

    federation-api: Add unstable support for MSC4373
    Co-authored-by: timedout <git@nexy7574.co.uk>

--- crates/ruma-federation-api/src/query.rs
+++ crates/ruma-federation-api/src/query.rs
@@ -1,5 +1,7 @@
 //! Endpoints to retrieve information from a homeserver about a resource.
 
 pub mod get_custom_information;
+#[cfg(feature = "unstable-msc4373")]
+pub mod get_edu_types;
 pub mod get_profile_information;
 pub mod get_room_information;
