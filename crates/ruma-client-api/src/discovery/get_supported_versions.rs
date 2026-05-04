d48e6227207c9c82b1a3506db3883fc538cedc7a
Jason Volk <jason@zemos.net>
2026-05-04 16:30:56 +0000

    client-api: Add support for MSC4383 (Client-Server Discovery of Server Version)
    Add the `unstable-msc4383` feature on `ruma-client-api` and `ruma`, and
    a `server` field on the `/versions` response. The `Server` carries the
    homeserver `name` and `version` with the same shape as the federation
    `/version` payload, and is absent when the server does not implement the
    proposal.
    
    Signed-off-by: Jason Volk <jason@zemos.net>

--- crates/ruma-client-api/src/discovery/get_supported_versions.rs
+++ crates/ruma-client-api/src/discovery/get_supported_versions.rs
@@ -10,6 +10,8 @@ use ruma_common::{
     api::{SupportedVersions, auth_scheme::AccessTokenOptional, request, response},
     metadata,
 };
+#[cfg(feature = "unstable-msc4383")]
+use serde::{Deserialize, Serialize};
 
 metadata! {
     method: GET,
@@ -35,6 +37,42 @@ pub struct Response {
     /// list might differ when an access token is provided.
     #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
     pub unstable_features: BTreeMap<String, bool>,
+
+    /// Information about the homeserver implementation, with the same shape as
+    /// the object returned by `GET /_matrix/federation/v1/version`.
+    ///
+    /// This uses the unstable prefix defined in [MSC4383].
+    ///
+    /// [MSC4383]: https://github.com/matrix-org/matrix-spec-proposals/pull/4383
+    #[cfg(feature = "unstable-msc4383")]
+    #[serde(rename = "net.zemos.msc4383.server", default, skip_serializing_if = "Option::is_none")]
+    pub server: Option<Server>,
+}
+
+/// Identifying information about the homeserver implementation.
+///
+/// This uses the unstable prefix defined in [MSC4383].
+///
+/// [MSC4383]: https://github.com/matrix-org/matrix-spec-proposals/pull/4383
+#[cfg(feature = "unstable-msc4383")]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[cfg_attr(not(ruma_unstable_exhaustive_types), non_exhaustive)]
+pub struct Server {
+    /// Arbitrary name that identifies this implementation.
+    pub name: String,
+
+    /// Version of this implementation.
+    ///
+    /// The version format depends on the implementation.
+    pub version: String,
+}
+
+#[cfg(feature = "unstable-msc4383")]
+impl Server {
+    /// Creates a `Server` with the given implementation `name` and `version`.
+    pub fn new(name: String, version: String) -> Self {
+        Self { name, version }
+    }
 }
 
 impl Request {
@@ -47,7 +85,12 @@ impl Request {
 impl Response {
     /// Creates a new `Response` with the given `versions`.
     pub fn new(versions: Vec<String>) -> Self {
-        Self { versions, unstable_features: BTreeMap::new() }
+        Self {
+            versions,
+            unstable_features: BTreeMap::new(),
+            #[cfg(feature = "unstable-msc4383")]
+            server: None,
+        }
     }
 
     /// Convert this `Response` into a [`SupportedVersions`] that can be used with
