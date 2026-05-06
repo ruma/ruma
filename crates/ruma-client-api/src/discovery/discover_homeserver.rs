6b0ddc6ce1b22d18da91486b081d3a45b52932db
Jason Volk <jason@zemos.net>
2026-05-06 01:36:03 -0700

    client-api: Unify RtcFocusInfo and RtcTransport into one type for MSC4143/4195

--- crates/ruma-client-api/src/discovery/discover_homeserver.rs
+++ crates/ruma-client-api/src/discovery/discover_homeserver.rs
@@ -4,20 +4,14 @@
 //!
 //! Get discovery information about the domain.
 
-#[cfg(feature = "unstable-msc4143")]
-use std::borrow::Cow;
-
-#[cfg(feature = "unstable-msc4143")]
-use ruma_common::serde::JsonObject;
 use ruma_common::{
     api::{auth_scheme::NoAccessToken, request, response},
     metadata,
 };
 use serde::{Deserialize, Serialize};
+
 #[cfg(feature = "unstable-msc4143")]
-use serde::{Deserializer, de, de::DeserializeOwned};
-#[cfg(feature = "unstable-msc4143")]
-use serde_json::Value as JsonValue;
+use crate::rtc::RtcTransport;
 
 metadata! {
     method: GET,
@@ -59,7 +53,7 @@ pub struct Response {
         default,
         skip_serializing_if = "Vec::is_empty"
     )]
-    pub rtc_foci: Vec<RtcFocusInfo>,
+    pub rtc_foci: Vec<RtcTransport>,
 }
 
 impl Request {
@@ -129,205 +123,5 @@ impl TileServerInfo {
     /// Creates a `TileServerInfo` with the given map style URL.
     pub fn new(map_style_url: String) -> Self {
         Self { map_style_url }
-    }
-}
-
-/// Information about a specific MatrixRTC focus.
-#[cfg(feature = "unstable-msc4143")]
-#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
-#[cfg_attr(not(ruma_unstable_exhaustive_types), non_exhaustive)]
-#[serde(tag = "type")]
-pub enum RtcFocusInfo {
-    /// A LiveKit RTC focus.
-    #[serde(rename = "livekit")]
-    LiveKit(LiveKitRtcFocusInfo),
-
-    /// A custom RTC focus.
-    #[doc(hidden)]
-    #[serde(untagged)]
-    _Custom(CustomRtcFocusInfo),
-}
-
-#[cfg(feature = "unstable-msc4143")]
-impl RtcFocusInfo {
-    /// A constructor to create a custom RTC focus.
-    ///
-    /// Prefer to use the public variants of `RtcFocusInfo` where possible; this constructor is
-    /// meant to be used for unsupported focus types only and does not allow setting arbitrary data
-    /// for supported ones.
-    ///
-    /// # Errors
-    ///
-    /// Returns an error if the `focus_type` is known and serialization of `data` to the
-    /// corresponding `RtcFocusInfo` variant fails.
-    pub fn new(focus_type: &str, data: JsonObject) -> serde_json::Result<Self> {
-        fn deserialize_variant<T: DeserializeOwned>(obj: JsonObject) -> serde_json::Result<T> {
-            serde_json::from_value(JsonValue::Object(obj))
-        }
-
-        Ok(match focus_type {
-            "livekit" => Self::LiveKit(deserialize_variant(data)?),
-            _ => Self::_Custom(CustomRtcFocusInfo { focus_type: focus_type.to_owned(), data }),
-        })
-    }
-
-    /// Creates a new `RtcFocusInfo::LiveKit`.
-    pub fn livekit(service_url: String) -> Self {
-        Self::LiveKit(LiveKitRtcFocusInfo { service_url })
-    }
-
-    /// Returns a reference to the focus type of this RTC focus.
-    pub fn focus_type(&self) -> &str {
-        match self {
-            Self::LiveKit(_) => "livekit",
-            Self::_Custom(custom) => &custom.focus_type,
-        }
-    }
-
-    /// Returns the associated data.
-    ///
-    /// The returned JSON object won't contain the `focus_type` field, please use
-    /// [`.focus_type()`][Self::focus_type] to access that.
-    ///
-    /// Prefer to use the public variants of `RtcFocusInfo` where possible; this method is meant to
-    /// be used for custom focus types only.
-    pub fn data(&self) -> Cow<'_, JsonObject> {
-        fn serialize<T: Serialize>(object: &T) -> JsonObject {
-            match serde_json::to_value(object).expect("rtc focus type serialization to succeed") {
-                JsonValue::Object(object) => object,
-                _ => panic!("all rtc focus types must serialize to objects"),
-            }
-        }
-
-        match self {
-            Self::LiveKit(info) => Cow::Owned(serialize(info)),
-            Self::_Custom(info) => Cow::Borrowed(&info.data),
-        }
-    }
-}
-
-#[cfg(feature = "unstable-msc4143")]
-impl<'de> Deserialize<'de> for RtcFocusInfo {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        use as_variant::as_variant;
-
-        let mut data = JsonObject::deserialize(deserializer)?;
-        let focus_type = data
-            .remove("type")
-            .and_then(|value| as_variant!(value, JsonValue::String))
-            .ok_or_else(|| de::Error::missing_field("type"))?;
-
-        match focus_type.as_ref() {
-            "livekit" => serde_json::from_value(data.into()).map(Self::LiveKit),
-            _ => Ok(Self::_Custom(CustomRtcFocusInfo { focus_type, data })),
-        }
-        .map_err(de::Error::custom)
-    }
-}
-
-/// Information about a LiveKit RTC focus.
-#[cfg(feature = "unstable-msc4143")]
-#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-#[cfg_attr(not(ruma_unstable_exhaustive_types), non_exhaustive)]
-pub struct LiveKitRtcFocusInfo {
-    /// The URL for the LiveKit service.
-    #[serde(rename = "livekit_service_url")]
-    pub service_url: String,
-}
-
-/// Information about a custom RTC focus type.
-#[doc(hidden)]
-#[cfg(feature = "unstable-msc4143")]
-#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
-pub struct CustomRtcFocusInfo {
-    /// The type of RTC focus.
-    #[serde(rename = "type")]
-    focus_type: String,
-
-    /// Remaining RTC focus data.
-    #[serde(flatten)]
-    data: JsonObject,
-}
-
-#[cfg(test)]
-mod tests {
-    #[cfg(feature = "unstable-msc4143")]
-    use assert_matches2::assert_let;
-    #[cfg(feature = "unstable-msc4143")]
-    use ruma_common::canonical_json::assert_to_canonical_json_eq;
-    #[cfg(feature = "unstable-msc4143")]
-    use serde_json::{from_value as from_json_value, json};
-
-    #[cfg(feature = "unstable-msc4143")]
-    use super::RtcFocusInfo;
-
-    #[test]
-    #[cfg(feature = "unstable-msc4143")]
-    fn test_livekit_rtc_focus_deserialization() {
-        // Given the JSON for a LiveKit RTC focus.
-        let json = json!({
-            "type": "livekit",
-            "livekit_service_url": "https://livekit.example.com"
-        });
-
-        // When deserializing it into an RtcFocusInfo.
-        let focus: RtcFocusInfo = from_json_value(json).unwrap();
-
-        // Then it should be recognized as a LiveKit focus with the correct service URL.
-        assert_let!(RtcFocusInfo::LiveKit(info) = focus);
-        assert_eq!(info.service_url, "https://livekit.example.com");
-    }
-
-    #[test]
-    #[cfg(feature = "unstable-msc4143")]
-    fn test_livekit_rtc_focus_serialization() {
-        // Given a LiveKit RTC focus info.
-        let focus = RtcFocusInfo::livekit("https://livekit.example.com".to_owned());
-
-        // When serializing to JSON, it should match the expected JSON structure.
-        assert_to_canonical_json_eq!(
-            focus,
-            json!({
-                "type": "livekit",
-                "livekit_service_url": "https://livekit.example.com"
-            })
-        );
-    }
-
-    #[test]
-    #[cfg(feature = "unstable-msc4143")]
-    fn test_custom_rtc_focus_serialization() {
-        // Given the JSON for a custom RTC focus type with additional fields.
-        let json = json!({
-            "type": "some-focus-type",
-            "additional-type-specific-field": "https://my_focus.domain",
-            "another-additional-type-specific-field": ["with", "Array", "type"]
-        });
-
-        // When deserializing it into an RtcFocusInfo.
-        let focus: RtcFocusInfo = from_json_value(json.clone()).unwrap();
-
-        // Then it should be recognized as a custom focus type, with all the additional fields
-        // included.
-        assert_eq!(focus.focus_type(), "some-focus-type");
-
-        let data = &focus.data();
-        assert_eq!(data["additional-type-specific-field"], "https://my_focus.domain");
-
-        let array_values: Vec<&str> = data["another-additional-type-specific-field"]
-            .as_array()
-            .unwrap()
-            .iter()
-            .map(|v| v.as_str().unwrap())
-            .collect();
-        assert_eq!(array_values, vec!["with", "Array", "type"]);
-
-        assert!(!data.contains_key("type"));
-
-        // When serializing it back to JSON, it should match the original JSON.
-        assert_to_canonical_json_eq!(focus, json);
     }
 }
--- crates/ruma-client-api/src/rtc.rs
+++ crates/ruma-client-api/src/rtc.rs
@@ -2,4 +2,216 @@
 //!
 //! [MatrixRTC]: https://github.com/matrix-org/matrix-spec-proposals/pull/4143
 
+use std::borrow::Cow;
+
+use ruma_common::serde::JsonObject;
+use serde::{Deserialize, Deserializer, Serialize};
+
 pub mod transports;
+
+/// Information about a specific MatrixRTC transport.
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
+#[cfg_attr(not(ruma_unstable_exhaustive_types), non_exhaustive)]
+#[serde(tag = "type")]
+pub enum RtcTransport {
+    /// A LiveKit RTC transport.
+    #[cfg(feature = "unstable-msc4195")]
+    #[serde(rename = "livekit")]
+    LiveKit(LiveKitRtcTransport),
+
+    /// A custom RTC transport.
+    #[doc(hidden)]
+    #[serde(untagged)]
+    _Custom(CustomRtcTransport),
+}
+
+impl<'de> Deserialize<'de> for RtcTransport {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        use serde::de::Error;
+
+        let mut obj = JsonObject::deserialize(deserializer)?;
+        let transport_type = match obj.remove("type") {
+            Some(serde_json::Value::String(s)) => s,
+            Some(_) => return Err(D::Error::custom("`type` must be a string")),
+            None => return Err(D::Error::missing_field("type")),
+        };
+
+        Ok(match transport_type.as_str() {
+            #[cfg(feature = "unstable-msc4195")]
+            "livekit" => Self::LiveKit(
+                serde_json::from_value(serde_json::Value::Object(obj)).map_err(D::Error::custom)?,
+            ),
+            _ => Self::_Custom(CustomRtcTransport { transport_type, data: obj }),
+        })
+    }
+}
+
+impl RtcTransport {
+    /// A constructor to create a custom RTC transport.
+    ///
+    /// Prefer to use the public variants of `RtcTransport` where possible; this constructor is
+    /// meant to be used for unsupported transport types only and does not allow setting arbitrary
+    /// data for supported ones.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the `transport_type` is known and serialization of `data` to the
+    /// corresponding `RtcTransport` variant fails.
+    pub fn new(transport_type: &str, data: JsonObject) -> serde_json::Result<Self> {
+        Ok(match transport_type {
+            #[cfg(feature = "unstable-msc4195")]
+            "livekit" => Self::LiveKit(serde_json::from_value(serde_json::Value::Object(data))?),
+            _ => Self::_Custom(CustomRtcTransport {
+                transport_type: transport_type.to_owned(),
+                data,
+            }),
+        })
+    }
+
+    #[cfg(feature = "unstable-msc4195")]
+    /// Creates a new `RtcTransport::LiveKit`.
+    pub fn livekit(service_url: String) -> Self {
+        Self::LiveKit(LiveKitRtcTransport { service_url })
+    }
+
+    /// Returns a reference to the transport type of this RTC transport.
+    pub fn transport_type(&self) -> &str {
+        match self {
+            #[cfg(feature = "unstable-msc4195")]
+            Self::LiveKit(_) => "livekit",
+            Self::_Custom(custom) => &custom.transport_type,
+        }
+    }
+
+    /// Returns the associated data.
+    ///
+    /// The returned JSON object won't contain the `type` field, please use
+    /// [`.transport_type()`][Self::transport_type] to access that.
+    ///
+    /// Prefer to use the public variants of `RtcTransport` where possible; this method is meant
+    /// to be used for custom transport types only.
+    pub fn data(&self) -> Cow<'_, JsonObject> {
+        #[cfg(feature = "unstable-msc4195")]
+        fn serialize<T: Serialize>(object: &T) -> JsonObject {
+            use serde_json::Value as JsonValue;
+
+            match serde_json::to_value(object).expect("rtc transport type serialization to succeed")
+            {
+                JsonValue::Object(object) => object,
+                _ => panic!("all rtc transport types must serialize to objects"),
+            }
+        }
+
+        match self {
+            #[cfg(feature = "unstable-msc4195")]
+            Self::LiveKit(info) => Cow::Owned(serialize(info)),
+            Self::_Custom(info) => Cow::Borrowed(&info.data),
+        }
+    }
+}
+
+/// Information about a LiveKit RTC transport.
+#[cfg(feature = "unstable-msc4195")]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[cfg_attr(not(ruma_unstable_exhaustive_types), non_exhaustive)]
+pub struct LiveKitRtcTransport {
+    /// The URL for the LiveKit service.
+    #[serde(rename = "livekit_service_url")]
+    pub service_url: String,
+}
+
+#[cfg(feature = "unstable-msc4195")]
+impl LiveKitRtcTransport {
+    /// Creates a new `LiveKitRtcTransport` with the given service URL.
+    pub fn new(service_url: String) -> Self {
+        Self { service_url }
+    }
+}
+
+#[cfg(feature = "unstable-msc4195")]
+impl From<LiveKitRtcTransport> for RtcTransport {
+    fn from(value: LiveKitRtcTransport) -> Self {
+        Self::LiveKit(value)
+    }
+}
+
+/// Information about a custom RTC transport.
+///
+/// This type does not implement `Deserialize` to prevent users from
+/// constructing the `_Custom` variant of [`RtcTransport`] for a known `type`.
+/// Deserialize through [`RtcTransport`] instead.
+#[doc(hidden)]
+#[derive(Clone, Debug, Serialize, PartialEq, Eq)]
+pub struct CustomRtcTransport {
+    /// The type of RTC transport.
+    #[serde(rename = "type")]
+    transport_type: String,
+
+    /// Remaining RTC transport data.
+    #[serde(flatten)]
+    data: JsonObject,
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_matches2::assert_matches;
+    use serde_json::{
+        Value as JsonValue, from_value as from_json_value, json, to_value as to_json_value,
+    };
+
+    use super::RtcTransport;
+
+    #[test]
+    fn serialize_roundtrip_custom_rtc_transport() {
+        let transport_type = "local.custom.transport";
+        assert_matches!(
+            json!({
+                "foo": "bar",
+                "baz": true,
+            }),
+            JsonValue::Object(transport_data)
+        );
+        let transport = RtcTransport::new(transport_type, transport_data.clone()).unwrap();
+        let json = json!({
+            "type": transport_type,
+            "foo": "bar",
+            "baz": true,
+        });
+
+        assert_eq!(transport.transport_type(), transport_type);
+        assert_eq!(*transport.data().as_ref(), transport_data);
+        assert_eq!(to_json_value(&transport).unwrap(), json);
+        assert_eq!(from_json_value::<RtcTransport>(json).unwrap(), transport);
+    }
+
+    #[cfg(feature = "unstable-msc4195")]
+    #[test]
+    fn livekit_transport_new_and_from_impl() {
+        use super::LiveKitRtcTransport;
+
+        let url = "http://livekit.local/".to_owned();
+        let inner = LiveKitRtcTransport::new(url.clone());
+        let transport = RtcTransport::from(inner);
+        assert_eq!(transport.transport_type(), "livekit");
+        assert_eq!(transport, RtcTransport::livekit(url));
+    }
+
+    #[cfg(feature = "unstable-msc4195")]
+    #[test]
+    fn serialize_roundtrip_livekit_sfu_transport() {
+        let transport_type = "livekit";
+        let livekit_service_url = "http://livekit.local/";
+        let transport = RtcTransport::livekit(livekit_service_url.to_owned());
+        let json = json!({
+            "type": transport_type,
+            "livekit_service_url": livekit_service_url,
+        });
+
+        assert_eq!(transport.transport_type(), transport_type);
+        assert_eq!(to_json_value(&transport).unwrap(), json);
+        assert_eq!(from_json_value::<RtcTransport>(json).unwrap(), transport);
+    }
+}
