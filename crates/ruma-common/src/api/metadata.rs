23f59b9c0260dcf387801b268410d4c557349d2b
Kévin Commaille <zecakeh@tedomum.fr>
2026-07-14 13:48:41 +0200

    api: Add MatrixVersion::V1_19

--- crates/ruma-common/src/api/metadata.rs
+++ crates/ruma-common/src/api/metadata.rs
@@ -388,6 +388,11 @@ pub enum MatrixVersion {
     ///
     /// See <https://spec.matrix.org/v1.18/>.
     V1_18,
+
+    /// Version 1.19 of the Matrix specification, released in Q2 2026.
+    ///
+    /// See <https://spec.matrix.org/v1.19/>.
+    V1_19,
 }
 
 impl TryFrom<&str> for MatrixVersion {
@@ -420,6 +425,7 @@ impl TryFrom<&str> for MatrixVersion {
             "v1.16" => V1_16,
             "v1.17" => V1_17,
             "v1.18" => V1_18,
+            "v1.19" => V1_19,
             _ => return Err(UnknownVersionError),
         })
     }
@@ -474,6 +480,7 @@ impl MatrixVersion {
             MatrixVersion::V1_16 => "v1.16",
             MatrixVersion::V1_17 => "v1.17",
             MatrixVersion::V1_18 => "v1.18",
+            MatrixVersion::V1_19 => "v1.19",
         };
 
         Some(string)
@@ -501,6 +508,7 @@ impl MatrixVersion {
             MatrixVersion::V1_16 => (1, 16),
             MatrixVersion::V1_17 => (1, 17),
             MatrixVersion::V1_18 => (1, 18),
+            MatrixVersion::V1_19 => (1, 19),
         }
     }
 
@@ -526,6 +534,7 @@ impl MatrixVersion {
             (1, 16) => Ok(MatrixVersion::V1_16),
             (1, 17) => Ok(MatrixVersion::V1_17),
             (1, 18) => Ok(MatrixVersion::V1_18),
+            (1, 19) => Ok(MatrixVersion::V1_19),
             _ => Err(UnknownVersionError),
         }
     }
@@ -620,7 +629,9 @@ impl MatrixVersion {
             // <https://spec.matrix.org/v1.17/rooms/#complete-list-of-room-versions>
             | MatrixVersion::V1_17
             // <https://spec.matrix.org/v1.18/rooms/#complete-list-of-room-versions>
-            | MatrixVersion::V1_18 => RoomVersionId::V12,
+            | MatrixVersion::V1_18
+            // <https://spec.matrix.org/v1.19/rooms/#complete-list-of-room-versions>
+            | MatrixVersion::V1_19 => RoomVersionId::V12,
         }
     }
 }
