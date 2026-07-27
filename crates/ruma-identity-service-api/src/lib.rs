c2284f308631f806ea0b705c9341aea37165956c
Kévin Commaille <zecakeh@tedomum.fr>
2026-07-27 11:58:40 +0200

    identity-service-api: Export nothing if neither the client nor server feature is active
    Because the crate is not useful without them.

--- crates/ruma-identity-service-api/src/lib.rs
+++ crates/ruma-identity-service-api/src/lib.rs
@@ -5,6 +5,9 @@
 //!
 //! [identity-api]: https://spec.matrix.org/v1.19/identity-service-api/
 
+// This crate is not useful without either of those features, so export nothing if they are not
+// enabled to avoid errors when running checks wrongly without enabling any of them.
+#![cfg(any(feature = "client", feature = "server"))]
 #![warn(missing_docs)]
 
 pub mod association;
