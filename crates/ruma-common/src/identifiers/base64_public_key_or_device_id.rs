520978ed541413a16261fc8b0bc2019e4685761e
Kévin Commaille <zecakeh@tedomum.fr>
2026-09-12 11:26:02 +0200

    identifiers: Use ruma_id macro for Base64PublicKeyOrDeviceId
    The `Base64PublicKeyOrDeviceId` dynamically sized type is removed and
    the `OwnedBase64PublicKeyOrDeviceId` type is renamed to
    `Base64PublicKeyOrDeviceId`.

--- crates/ruma-common/src/identifiers/base64_public_key_or_device_id.rs
+++ crates/ruma-common/src/identifiers/base64_public_key_or_device_id.rs
@@ -1,6 +1,6 @@
-use ruma_macros::IdDst;
+use ruma_macros::ruma_id;
 
-use super::{Base64PublicKey, DeviceId, IdParseError, KeyName, OwnedDeviceId};
+use super::{Base64PublicKey, IdParseError, KeyName, OwnedDeviceId};
 
 /// A Matrix ID that can be either a [`DeviceId`] or a [`Base64PublicKey`].
 ///
@@ -14,17 +14,15 @@ use super::{Base64PublicKey, DeviceId, IdParseError, KeyName, OwnedDeviceId};
 /// # Example
 ///
 /// ```
-/// use ruma_common::{Base64PublicKeyOrDeviceId, OwnedBase64PublicKeyOrDeviceId};
+/// use ruma_common::Base64PublicKeyOrDeviceId;
 ///
-/// let ref_id: &Base64PublicKeyOrDeviceId = "abcdefghi".into();
-/// assert_eq!(ref_id.as_str(), "abcdefghi");
-///
-/// let owned_id: OwnedBase64PublicKeyOrDeviceId = "ijklmnop".into();
-/// assert_eq!(owned_id.as_str(), "ijklmnop");
+/// let id: Base64PublicKeyOrDeviceId = "abcdefghi".into();
+/// assert_eq!(id, "abcdefghi");
 /// ```
-#[repr(transparent)]
-#[derive(PartialEq, Eq, PartialOrd, Ord, Hash, IdDst)]
-pub struct Base64PublicKeyOrDeviceId(str);
+///
+/// [`DeviceId`]: super::DeviceId
+#[ruma_id]
+pub struct Base64PublicKeyOrDeviceId;
 
 impl KeyName for Base64PublicKeyOrDeviceId {
     fn validate(_s: &str) -> Result<(), IdParseError> {
@@ -32,31 +30,13 @@ impl KeyName for Base64PublicKeyOrDeviceId {
     }
 }
 
-impl KeyName for OwnedBase64PublicKeyOrDeviceId {
-    fn validate(_s: &str) -> Result<(), IdParseError> {
-        Ok(())
-    }
-}
-
-impl<'a> From<&'a DeviceId> for &'a Base64PublicKeyOrDeviceId {
-    fn from(value: &'a DeviceId) -> Self {
-        Self::from(value.as_str())
-    }
-}
-
-impl From<OwnedDeviceId> for OwnedBase64PublicKeyOrDeviceId {
+impl From<OwnedDeviceId> for Base64PublicKeyOrDeviceId {
     fn from(value: OwnedDeviceId) -> Self {
         unsafe { Self::from_inner_unchecked(value.into_inner()) }
     }
 }
 
-impl<'a> From<&'a Base64PublicKey> for &'a Base64PublicKeyOrDeviceId {
-    fn from(value: &'a Base64PublicKey) -> Self {
-        Self::from(value.as_str())
-    }
-}
-
-impl From<Base64PublicKey> for OwnedBase64PublicKeyOrDeviceId {
+impl From<Base64PublicKey> for Base64PublicKeyOrDeviceId {
     fn from(value: Base64PublicKey) -> Self {
         unsafe { Self::from_inner_unchecked(value.into_inner()) }
     }
@@ -64,21 +44,21 @@ impl From<Base64PublicKey> for OwnedBase64PublicKeyOrDeviceId {
 
 #[cfg(test)]
 mod tests {
-    use super::OwnedBase64PublicKeyOrDeviceId;
+    use super::Base64PublicKeyOrDeviceId;
     use crate::{Base64PublicKey, OwnedDeviceId};
 
     #[test]
-    fn convert_owned_device_id_to_owned_base64_public_key_or_device_id() {
+    fn convert_owned_device_id_to_base64_public_key_or_device_id() {
         let device_id: OwnedDeviceId = "MYDEVICE".into();
-        let mixed: OwnedBase64PublicKeyOrDeviceId = device_id.into();
+        let mixed: Base64PublicKeyOrDeviceId = device_id.into();
 
         assert_eq!(mixed, "MYDEVICE");
     }
 
     #[test]
-    fn convert_owned_base64_public_key_to_owned_base64_public_key_or_device_id() {
+    fn convert_base64_public_key_to_base64_public_key_or_device_id() {
         let base64_public_key: Base64PublicKey = "base64+master+public+key".try_into().unwrap();
-        let mixed: OwnedBase64PublicKeyOrDeviceId = base64_public_key.into();
+        let mixed: Base64PublicKeyOrDeviceId = base64_public_key.into();
 
         assert_eq!(mixed, "base64+master+public+key");
     }
