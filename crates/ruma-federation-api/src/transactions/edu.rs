0678a0e4bbf5b1bff08a33bf949174cf860e1eeb
Kévin Commaille <zecakeh@tedomum.fr>
2026-09-20 16:46:50 +0200

    federation-api: Replace assert_matches2 crate with assert_matches

--- crates/ruma-federation-api/src/transactions/edu.rs
+++ crates/ruma-federation-api/src/transactions/edu.rs
@@ -390,7 +390,7 @@ pub struct CustomEdu {
 
 #[cfg(test)]
 mod tests {
-    use assert_matches2::assert_matches;
+    use assert_matches::assert_matches;
     use js_int::uint;
     use ruma_common::{canonical_json::assert_to_canonical_json_eq, presence::PresenceState};
     use ruma_events::ToDeviceEventType;
